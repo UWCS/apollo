3df833fa3a0d02ab81d70da6763fd3f8f5f36aea
David Richardson <david@tankski.co.uk>
2018-02-15 12:44:50 +0000

    Move verify command to Cog system

--- commands/verify.py
+++ commands/verify.py
@@ -4,12 +4,12 @@ from datetime import datetime
 import requests
 from discord.abc import PrivateChannel
 from discord.ext import commands
-from discord.ext.commands import CommandError, Context, check
+from discord.ext.commands import CommandError, Context, check, Bot
 
 from config import CONFIG
 from models import User, db_session
 
-LONG_HELP_TEXT = f"""
+LONG_HELP_TEXT = """
 Allows you to verify your account with your university number to gain the 'CompSoc Member' role. Should be sent in a private message.
 """
 
@@ -37,74 +37,82 @@ def is_private_channel():
     return check(predicate)
 
 
-@commands.command(ignore_extra=True, help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
-@is_private_channel()
-async def verify(ctx: Context, uni_number: str):
-    # Check that the university number provided is actually formatted correctly
-    uni_id_regex = re.compile(r'[0-9]{7}')
-    if not re.match(uni_id_regex, uni_number):
-        raise VerifyError(message='\'{id}\' is not a valid university number.'.format(id=uni_number))
+class Verify():
+    def __init__(self, bot: Bot):
+        self.bot = bot
 
-    # Get the discord data from our servers
-    headers = {'Authorization': 'Token {token}'.format(token=CONFIG['UWCS_API_TOKEN'])}
-    api_request = requests.get('https://uwcs.co.uk/accounts/api/{uni_id}/'.format(uni_id=uni_number), headers=headers)
+    @commands.command(ignore_extra=True, help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
+    @is_private_channel()
+    async def verify(self, ctx: Context, uni_number: str):
+        # Check that the university number provided is actually formatted correctly
+        uni_id_regex = re.compile(r'[0-9]{7}')
+        if not re.match(uni_id_regex, uni_number):
+            raise VerifyError(message='\'{id}\' is not a valid university number.'.format(id=uni_number))
 
-    # If the request goes okay
-    if api_request.status_code == 200:
-        api_username = api_request.json()['discord_user']
-        if not api_username:
-            # Tell the user they haven't set their discord tag on the website
-            raise VerifyError(
-                message='Your Discord tag has not been set on the UWCS website - it can be set under your account '
-                        'settings: https://uwcs.co.uk/accounts/profile/update/')
+        # Get the discord data from our servers
+        headers = {'Authorization': 'Token {token}'.format(token=CONFIG['UWCS_API_TOKEN'])}
+        api_request = requests.get('https://uwcs.co.uk/accounts/api/{uni_id}/'.format(uni_id=uni_number),
+                                   headers=headers)
+
+        # If the request goes okay
+        if api_request.status_code == 200:
+            api_username = api_request.json()['discord_user']
+            if not api_username:
+                # Tell the user they haven't set their discord tag on the website
+                raise VerifyError(
+                    message='Your Discord tag has not been set on the UWCS website - it can be set under your account '
+                            'settings: https://uwcs.co.uk/accounts/profile/update/')
+            else:
+                # This *shouldn't* happen but in the small case it may, just get the user to try again. Yay async systems!
+                user = db_session.query(User).filter(User.user_uid == ctx.message.author.id).first()
+                if not user:
+                    raise VerifyError(
+                        message='We\'ve hit a snag verifying your account - please try again in a few minutes!')
+
+                # Check if the user has already verified
+                if user.uni_id == uni_number:
+                    raise VerifyError(message='You have already verified this university number.')
+
+                # Check they are who they say they are
+                if not api_username == str(ctx.message.author):
+                    raise VerifyError(
+                        message='The user you\'re trying to verify doesn\'t match the tag associated with your university '
+                                'ID - please make sure you\'ve set your tag correctly and try again.')
+
+                # Get all of the objects necessary to apply the roles
+                compsoc_guild = [guild for guild in ctx.bot.guilds if guild.id == CONFIG['UWCS_DISCORD_ID']][0]
+                compsoc_member = compsoc_guild.get_member(ctx.message.author.id)
+                if not compsoc_member:
+                    raise VerifyError(
+                        message='It seems like you\'re not a member of the UWCS Discord yet. You can join us here: '
+                                'https://discordapp.com/invite/qhnbSyh')
+                try:
+                    compsoc_role = [role for role in compsoc_guild.roles if role.id == CONFIG['UWCS_MEMBER_ROLE_ID']][0]
+                except IndexError:
+                    raise VerifyError(
+                        message='I can\'t find the role to give you on the UWCS Discord. Let one of the exec or admins '
+                                'know so they can fix this problem!')
+
+                # Give them the role and let them know
+                await compsoc_member.add_roles(compsoc_role,
+                                               reason='User verified with the university number of {uni_id}'.format(
+                                                   uni_id=uni_number))
+                user.uni_id = uni_number
+                user.verified_at = datetime.utcnow()
+                db_session.commit()
+
+                await ctx.send(
+                    'You\'re all verified and ready to go! Welcome to the UWCS Discord.')
         else:
-            # This *shouldn't* happen but in the small case it may, just get the user to try again. Yay async systems!
-            user = db_session.query(User).filter(User.user_uid == ctx.message.author.id).first()
-            if not user:
-                raise VerifyError(
-                    message='We\'ve hit a snag verifying your account - please try again in a few minutes!')
+            raise VerifyError(
+                message='That university number appears to be inactive or not exist - if you have just purchased '
+                        'membership please give the system 5 minutes to create an account. If you\'re not a member of the '
+                        'society you can purchase membership through the University of Warwick Student\'s union.')
 
-            # Check if the user has already verified
-            if user.uni_id == uni_number:
-                raise VerifyError(message='You have already verified this university number.')
-
-            # Check they are who they say they are
-            if not api_username == str(ctx.message.author):
-                raise VerifyError(
-                    message='The user you\'re trying to verify doesn\'t match the tag associated with your university '
-                            'ID - please make sure you\'ve set your tag correctly and try again.')
-
-            # Get all of the objects necessary to apply the roles
-            compsoc_guild = [guild for guild in ctx.bot.guilds if guild.id == CONFIG['UWCS_DISCORD_ID']][0]
-            compsoc_member = compsoc_guild.get_member(ctx.message.author.id)
-            if not compsoc_member:
-                raise VerifyError(
-                    message='It seems like you\'re not a member of the UWCS Discord yet. You can join us here: '
-                            'https://discordapp.com/invite/qhnbSyh')
-            try:
-                compsoc_role = [role for role in compsoc_guild.roles if role.id == CONFIG['UWCS_MEMBER_ROLE_ID']][0]
-            except IndexError:
-                raise VerifyError(
-                    message='I can\'t find the role to give you on the UWCS Discord. Let one of the exec or admins '
-                            'know so they can fix this problem!')
-
-            # Give them the role and let them know
-            await compsoc_member.add_roles(compsoc_role,
-                                           reason='User verified with the university number of {uni_id}'.format(
-                                               uni_id=uni_number))
-            user.uni_id = uni_number
-            user.verified_at = datetime.utcnow()
-            db_session.commit()
-
-            await ctx.send(
-                'You\'re all verified and ready to go! Welcome to the UWCS Discord.')
-    else:
-        raise VerifyError(
-            message='That university number appears to be inactive or not exist - if you have just purchased '
-                    'membership please give the system 5 minutes to create an account. If you\'re not a member of the '
-                    'society you can purchase membership through the University of Warwick Student\'s union.')
+    @verify.error
+    async def verify_error_handler(self, ctx: Context, error: VerifyError):
+        await ctx.send(error.message)
 
 
-@verify.error
-async def verify_error_handler(ctx: Context, error: VerifyError):
-    await ctx.send(error.message)
+def setup(bot: Bot):
+    bot.add_cog(Verify(bot))
