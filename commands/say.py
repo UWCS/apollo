dbf0831f801ec4bc86fd9705fba9877763ff88a5
David Richardson <david@tankski.co.uk>
2018-02-23 21:59:11 +0000

    Fix formatting to be PEP8 compliant

--- commands/say.py
+++ commands/say.py
@@ -1,13 +1,5 @@
-import re
-from datetime import datetime
-
-import requests
-from discord.abc import PrivateChannel
 from discord.ext import commands
-from discord.ext.commands import CommandError, Context, check, Bot, clean_content
-
-from config import CONFIG
-from models import User, db_session
+from discord.ext.commands import Context, Bot, clean_content
 
 LONG_HELP_TEXT = """
 Make the bot repeat after you.
@@ -15,13 +7,15 @@ Make the bot repeat after you.
 
 SHORT_HELP_TEXT = """Make the bot repeat after you."""
 
+
 class Say:
     def __init__(self, bot: Bot):
         self.bot = bot
 
     @commands.command(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
     async def say(self, ctx: Context, *message: clean_content):
-       await ctx.send(" ".join([x.lstrip('@') for x in message])) 
+        await ctx.send(" ".join([x.lstrip('@') for x in message]))
+
 
 def setup(bot: Bot):
     bot.add_cog(Say(bot))
