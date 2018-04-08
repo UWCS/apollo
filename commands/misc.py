f7fb12f8261eec30b959f6f55412e89eb73d0881
Jai Sharma <jai.p.sharma1@gmail.com>
2018-04-08 01:04:01 +0100

    Added faux command

--- commands/misc.py
+++ commands/misc.py
@@ -2,6 +2,7 @@ from discord.ext import commands
 from discord.ext.commands import Context, Bot
 
 ZED0_HELP_TEXT = """Very important command."""
+FAUX_HELP_TEXT = """A member of the rust evangelical strike force."""
 
 
 class Misc:
@@ -11,6 +12,10 @@ class Misc:
     @commands.command(help=ZED0_HELP_TEXT, brief=ZED0_HELP_TEXT)
     async def zed0(self, ctx: Context):
         await ctx.send("¬_¬")
+
+    @commands.command(help=FAUX_HELP_TEXT, brief=FAUX_HELP_TEXT)
+    async def faux(self, ctx: Context):
+        await ctx.send("RUST")
 
 
 def setup(bot: Bot):
