5b2267deddb751efa7a9e13f7afc0b759c5a35db
MADOS\A <bitbucket@ameliewd.com>
2019-02-03 23:07:37 +0000

    Added facts
    Added the facts command
    Removed !sam
    Modified !blueshell message

--- apollo.py
+++ apollo.py
@@ -25,7 +25,7 @@ GLHF! :rocket:
 
 # The command extensions to be loaded by the bot
 EXTENSIONS = ['commands.verify', 'commands.karma', 'commands.say', 'commands.flip', 'commands.misc', 'commands.admin',
-              'commands.blacklist']
+              'commands.blacklist', 'commands.facts']
 
 bot = Bot(command_prefix=when_mentioned_or('!'), description=DESCRIPTION)
 
