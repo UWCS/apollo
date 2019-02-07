8f28a72bec542ef054f371b4160c6f11112dd07f
Rhiannon Michelmore <rhiannon.michelmore@gmail.com>
2019-02-07 19:41:21 +0000

    missed one part, forgot to alter message content correctly for irc users

--- apollo.py
+++ apollo.py
@@ -101,6 +101,15 @@ async def on_message(message: Message):
             if reply:
                 await message.channel.send(reply)
 
+    # allow irc users to use commands by altering content to remove the nick before sending for command processing
+    # note that clean_content is *not* altered and everything relies on this fact for it to work without having to go back and lookup the message in the db
+    # if message.content.startswith("**<"): <-- FOR TESTING
+    if message.author.id == CONFIG['UWCS_DISCORD_BRIDGE_BOT_ID']:
+        # Search for first "> " and strip the message from there (Since irc nicks cant have <, > in them
+        idx = message.content.find('>** ')
+        idx += 4
+        message.content = message.content[idx:]
+
     await bot.process_commands(message)
 
 
