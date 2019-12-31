51c65305c18aff01ea8e8b8367c257ade2dd6bd5
John Blundell <jlobblet-github@gmx.co.uk>
2019-12-31 21:36:48 +0000

    Fix black formatting, simplify regex
    My black was set to -l 72, producing a different output. This has been run through -l 88.
    Regex changes: `(?:\s+)?` -> `\s*`

--- commands/reminders.py
+++ commands/reminders.py
@@ -55,25 +55,17 @@ def parse_time(time):
             pass
 
     if not parsed_time:
-        result = re.match(r"(\d+d)?(?:\s+)?(\d+h)?(?:\s+)?(\d+m)?(?:\s+)?(\d+s)?(?!^)$", time)
+        result = re.match(r"(\d+d)?\s*(\d+h)?\s*(\d+m)?\s*(\d+s)?(?!^)$", time)
         if result:
             parsed_time = now
             if result.group(1):
-                parsed_time = parsed_time + timedelta(
-                    days=int(result.group(1)[:-1])
-                )
+                parsed_time = parsed_time + timedelta(days=int(result.group(1)[:-1]))
             if result.group(2):
-                parsed_time = parsed_time + timedelta(
-                    hours=int(result.group(2)[:-1])
-                )
+                parsed_time = parsed_time + timedelta(hours=int(result.group(2)[:-1]))
             if result.group(3):
-                parsed_time = parsed_time + timedelta(
-                    minutes=int(result.group(3)[:-1])
-                )
+                parsed_time = parsed_time + timedelta(minutes=int(result.group(3)[:-1]))
             if result.group(4):
-                parsed_time = parsed_time + timedelta(
-                    seconds=int(result.group(4)[:-1])
-                )
+                parsed_time = parsed_time + timedelta(seconds=int(result.group(4)[:-1]))
 
     return parsed_time
 
