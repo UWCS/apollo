16c4ec1dcdbdbae305469aaf588f624d461e7941
David Richardson <david@tankski.co.uk>
2020-06-14 15:00:47 +0100

    Change cost to a profile-based characteristic
    Signed-off-by: David Richardson <david@tankski.co.uk>

--- models.py
+++ models.py
@@ -12,7 +12,6 @@ from sqlalchemy import (
     func,
     Boolean,
     Text,
-    Float,
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.hybrid import hybrid_property
@@ -155,12 +154,21 @@ class User(Base):
 
 @auto_str
 class FilamentType(Base):
+    FILLAMENTUM = "fillamentum"
+    PRUSAMENT = "prusament"
+
     __tablename__ = "filament_types"
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(Text, nullable=False, unique=True)
-    cost = Column(Float, nullable=False)
+    profile = Column(String, default=FILLAMENTUM)
     image_path = Column(String, nullable=False, unique=True)
+
+    @staticmethod
+    def verify_type(type_str: str) -> bool:
+        return {FilamentType.FILLAMENTUM: True, FilamentType.PRUSAMENT: True}.get(
+            type_str.lower(), False
+        )
 
 
 @auto_str
