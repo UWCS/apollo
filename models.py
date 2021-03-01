7839436fa34cf952637d01966587c5905ddeebd0
Jlobblet <jlobblet-github@gmx.co.uk>
2021-03-01 22:14:01 +0000

    Add model for mini_karma_channels

--- models.py
+++ models.py
@@ -181,6 +181,15 @@ class IgnoredChannel(Base):
 
 
 @auto_str
+class MiniKarmaChannel(Base):
+    __tablename__ = "mini_karma_channels"
+
+    channel = Column(BigInteger, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    added_at = Column(DateTime, nullable=False, default=func.current_timestamp())
+
+
+@auto_str
 class BlockedKarma(Base):
     __tablename__ = "blacklist"
 
