8a87a0630914ba95159a3d76b99bea3238cbf7de
Jlobblet <jlobblet-github@gmx.co.uk>
2021-03-01 23:33:47 +0000

    Respect channel minikarma settings

--- karma/karma.py
+++ karma/karma.py
@@ -6,9 +6,10 @@ from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 from sqlalchemy_utils import ScalarListException
 
+from cogs.commands.admin import MiniKarmaMode
 from config import CONFIG
-from karma.parser import create_transactions, parse_message
-from models import Karma, KarmaChange, User
+from karma.parser import KarmaTransaction, create_transactions, parse_message
+from models import Karma, KarmaChange, MiniKarmaChannel, User
 from utils.aliases import get_name_string
 
 
@@ -36,14 +37,88 @@ def process_karma(message: Message, message_id: int, db_session: Session, timeou
     # Get karma-ing user
     user = db_session.query(User).filter(User.user_uid == message.author.id).first()
 
+    # Get whether the channel is on mini karma or not
+    channel = db_session.query(MiniKarmaChannel).filter(MiniKarmaChannel.channel == message.channel.id).one_or_none()
+    if channel is None:
+        karma_mode = MiniKarmaMode.Normal
+    else:
+        karma_mode = MiniKarmaMode.Mini
+
+    def own_karma_error(name):
+        if karma_mode == MiniKarmaMode.Normal:
+            return f' • Could not change "{name}" because you cannot change your own karma! :angry:'
+        else:
+            return f'could not change "**{name}**" (own name)'
+
+    def internal_error(name):
+        if karma_mode == MiniKarmaMode.Normal:
+            return f' • Could not create "{name}" due to an internal error.'
+        else:
+            return f'could not change "**{name}**" (internal error)'
+
+    def cooldown_error(name, td):
+        # Tell the user that the item is on cooldown
+        if td.seconds < 60:
+            seconds_plural = f"second{'s' if td.seconds != 1 else ''}"
+            duration = f"{td.seconds} {seconds_plural}"
+        else:
+            mins = td.seconds // 60
+            mins_plural = f"minute{'s' if mins != 1 else ''}"
+            duration = f"{mins} {mins_plural}"
+
+        if karma_mode == MiniKarmaMode.Normal:
+            return f' • Could not change "{name}" since it is still on cooldown (last altered {duration} ago).\n'
+        else:
+            return f'could not change "**{name}**" (cooldown, last edit {duration} ago)'
+
+    def success_item(tr: KarmaTransaction):
+        # Give some sass if someone is trying to downvote the bot
+        if tr.name.casefold() == "apollo" and tr.net_karma < 0:
+            apollo_response = ":wink:"
+        else:
+            apollo_response = ""
+
+        if tr.net_karma == 1:
+            op = "++"
+        elif tr.net_karma == -1:
+            op = "--"
+        else:
+            op = "+-"
+
+        # Build the karma item string
+        if tr.reasons:
+            if len(tr.reasons) > 1:
+                reasons_plural = "reasons"
+                reasons_has = "have"
+            else:
+                reasons_plural = "reason"
+                reasons_has = "has"
+
+            if karma_mode == MiniKarmaMode.Normal:
+                if tr.self_karma:
+                    return f" • **{truncated_name}** (new score is {karma_change.score}) and your {reasons_plural} {reasons_has} been recorded. *Fool!* that's less karma to you. :smiling_imp:"
+                else:
+                    return f" • **{truncated_name}** (new score is {karma_change.score}) and your {reasons_plural} {reasons_has} been recorded. {apollo_response}"
+            else:
+                return f"**{truncated_name}{op}** (now {karma_change.score}, {reasons_plural} recorded)"
+
+        else:
+            if karma_mode == MiniKarmaMode.Normal:
+                if tr.self_karma:
+                    return f" • **{truncated_name}** (new score is {karma_change.score}). *Fool!* that's less karma to you. :smiling_imp:"
+                else:
+                    return f" • **{truncated_name}** (new score is {karma_change.score}). {apollo_response}"
+            else:
+                return f"**{truncated_name}{op}** (now {karma_change.score})"
+
     # Start preparing the reply string
     if len(transactions) > 1:
         transaction_plural = "s"
     else:
         transaction_plural = ""
 
-    item_str = ""
-    error_str = ""
+    items = []
+    errors = []
 
     # Iterate over the transactions to write them to the database
     for transaction in transactions:
@@ -56,7 +131,7 @@ def process_karma(message: Message, message_id: int, db_session: Session, timeou
 
         # Catch any self-karma transactions early
         if transaction.self_karma and transaction.net_karma > -1:
-            error_str += f' • Could not change "{truncated_name}" because you cannot change your own karma! :angry:\n'
+            errors.append(own_karma_error(truncated_name))
             continue
 
         # Get the karma item from the database if it exists
@@ -74,7 +149,7 @@ def process_karma(message: Message, message_id: int, db_session: Session, timeou
                 db_session.commit()
             except (ScalarListException, SQLAlchemyError):
                 db_session.rollback()
-                error_str += f' • Could not create "{truncated_name}" due to an internal error.\n'
+                errors.append(internal_error(truncated_name))
                 continue
 
         # Get the last change (or none if there was none)
@@ -106,7 +181,7 @@ def process_karma(message: Message, message_id: int, db_session: Session, timeou
                 db_session.commit()
             except (ScalarListException, SQLAlchemyError):
                 db_session.rollback()
-                error_str += f' • Could not change "{truncated_name}" due to an internal error.\n'
+                errors.append(internal_error(truncated_name))
                 continue
         else:
             time_delta = datetime.utcnow() - last_change.created_at
@@ -132,20 +207,10 @@ def process_karma(message: Message, message_id: int, db_session: Session, timeou
                     db_session.commit()
                 except (ScalarListException, SQLAlchemyError):
                     db_session.rollback()
-                    error_str += f' • Could not change "{truncated_name}" due to an internal error.\n'
+                    errors.append(internal_error(truncated_name))
                     continue
             else:
-                # Tell the user that the item is on cooldown
-                if time_delta.seconds < 60:
-                    seconds_plural = f"second{'s' if time_delta.seconds != 1 else ''}"
-                    duration = f"{time_delta.seconds} {seconds_plural}"
-                else:
-                    mins = time_delta.seconds // 60
-                    mins_plural = f"minute{'s' if mins != 1 else ''}"
-                    duration = f"{mins} {mins_plural}"
-
-                error_str += f' • Could not change "{truncated_name}" since it is still on cooldown (last altered {duration} ago).\n'
-
+                errors.append(cooldown_error(truncated_name, time_delta))
                 continue
 
         # Update karma counts
@@ -160,44 +225,28 @@ def process_karma(message: Message, message_id: int, db_session: Session, timeou
             else:
                 karma_item.minuses = karma_item.minuses + 1
 
-        # Give some sass if someone is trying to downvote the bot
-        if transaction.name.casefold() == "apollo" and transaction.net_karma < 0:
-            apollo_response = ":wink:"
-        else:
-            apollo_response = ""
-
-        # Build the karma item string
-        if transaction.reasons:
-            if len(transaction.reasons) > 1:
-                reasons_plural = "s"
-                reasons_has = "have"
-            else:
-                reasons_plural = ""
-                reasons_has = "has"
-
-            if transaction.self_karma:
-                item_str += f" • **{truncated_name}** (new score is {karma_change.score}) and your reason{reasons_plural} {reasons_has} been recorded. *Fool!* that's less karma to you. :smiling_imp:\n"
-            else:
-                item_str += f" • **{truncated_name}** (new score is {karma_change.score}) and your reason{reasons_plural} {reasons_has} been recorded. {apollo_response}\n"
-        else:
-            if transaction.self_karma:
-                item_str += f" • **{truncated_name}** (new score is {karma_change.score}). *Fool!* that's less karma to you. :smiling_imp:\n"
-            else:
-                item_str += f" • **{truncated_name}** (new score is {karma_change.score}). {apollo_response}\n"
+        items.append(success_item(transaction))
 
     # Get the name, either from discord or irc
     author_display = get_name_string(message)
 
     # Construct the reply string in totality
     # If you have error(s) and no items processed successfully
-    if not item_str and error_str:
-        reply = f"Sorry {author_display}, I couldn't karma the requested item{transaction_plural} because of the following problem{transaction_plural}:\n\n{error_str}"
-    # If you have items processed successfully but some errors too
-    elif item_str and error_str:
-        reply = f"Thanks {author_display}, I have made changes to the following item(s) karma:\n\n{item_str}\n\nThere were some issues with the following item(s), too:\n\n{error_str}"
-    # If all items were processed successfully
+    if karma_mode == MiniKarmaMode.Normal:
+        item_str = "\n".join(items)
+        error_str = "\n".join(errors)
+        if not item_str and error_str:
+            reply = f"Sorry {author_display}, I couldn't karma the requested item{transaction_plural} because of the following problem{transaction_plural}:\n\n{error_str}"
+        # If you have items processed successfully but some errors too
+        elif item_str and error_str:
+            reply = f"Thanks {author_display}, I have made changes to the following item(s) karma:\n\n{item_str}\n\nThere were some issues with the following item(s), too:\n\n{error_str}"
+        # If all items were processed successfully
+        else:
+            reply = f"Thanks {author_display}, I have made changes to the following karma item{transaction_plural}:\n\n{item_str}"
     else:
-        reply = f"Thanks {author_display}, I have made changes to the following karma item{transaction_plural}:\n\n{item_str}"
+        item_str = " ".join(items)
+        error_str = " ".join(errors)
+        reply = " ".join(filter(None, ["Changes:", item_str, error_str]))
 
     # Commit any changes (in case of any DB inconsistencies)
     try:
