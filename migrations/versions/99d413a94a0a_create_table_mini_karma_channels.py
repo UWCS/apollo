7b54fc8a2b188d575eb592aee6571f95ade77929
Jlobblet <jlobblet-github@gmx.co.uk>
2021-03-01 22:12:54 +0000

    Add create_table_mini_karma_channels revision

---
+++ migrations/versions/99d413a94a0a_create_table_mini_karma_channels.py
@@ -0,0 +1,29 @@
+"""Create table mini_karma_channels
+
+Revision ID: 99d413a94a0a
+Revises: 36b213001624
+Create Date: 2021-03-01 22:09:44.349012
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '99d413a94a0a'
+down_revision = '36b213001624'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "mini_karma_channels",
+        sa.Column("channel", sa.BigInteger, primary_key=True, nullable=False),
+        sa.Column("user_id", sa.Integer, sa.ForeignKey("users.id"), nullable=False),
+        sa.Column("added_at", sa.DateTime, nullable=False, default=sa.func.current_timestamp())
+    )
+
+
+def downgrade():
+    op.drop_table("mini_karma_channels")
