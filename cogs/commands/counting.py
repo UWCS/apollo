783864b22b88d20f844f77b7ae39829eba373d3a
Jlobblet <jlobblet-github@gmx.co.uk>
2021-03-06 02:09:42 +0000

    Move counting to group

--- cogs/commands/counting.py
+++ cogs/commands/counting.py
@@ -1,6 +1,6 @@
 from decimal import Decimal
 
-from discord.ext.commands import Bot, Cog, Context, check, command
+from discord.ext.commands import Bot, Cog, Context, check, command, group
 
 from utils import is_decimal
 
@@ -17,59 +17,60 @@ class Counting(Cog):
         self.currently_playing = False
         self.channel = None
 
-    @command(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
+    @group(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
     async def counting(self, ctx: Context):
-        if self.currently_playing:
-            channel = (
-                "this channel"
-                if self.channel == ctx.message.channel
-                else self.channel.mention
-            )
-            await ctx.send(f"There is already a game being played in {channel}!")
-            return
-
-        self.currently_playing = True
-        channel = ctx.message.channel
-
-        await ctx.send(f"The game begins!")
-        # The count starts at 0.
-        count = 0
-        # The number of successful replies in a row.
-        length = 0
-        # We need to determine what the step is.
-        # It will be the first decimal number sent in the same channel.
-
-        def check_dec(m):
-            return m.channel == channel and is_decimal(m.content)
-
-        msg = await self.bot.wait_for("message", check=check)
-        # Set the step.
-        await msg.add_reaction("✅")
-        step = Decimal(msg.content)
-        length += 1
-        count += step
-
-        while True:
-            # Wait for the next numeric message
-            msg = await self.bot.wait_for("message", check=check_dec)
-            value = Decimal(msg.content)
-            if value == count + step:
-                # If the number is correct, increase the count and length.
-                count += step
-                length += 1
-                await msg.add_reaction("✅")
-            else:
-                # Otherwise, break the chain.
-                await msg.add_reaction("❌")
-                await ctx.send(
-                    f"Gone wrong at {count}! The next number was {count + step}.\n"
-                    f"This chain lasted {length} consecutive messages."
+        if not ctx.invoked_subcommand:
+            if self.currently_playing:
+                channel = (
+                    "this channel"
+                    if self.channel == ctx.message.channel
+                    else self.channel.mention
                 )
-                break
+                await ctx.send(f"There is already a game being played in {channel}!")
+                return
 
-        # Reset the cog's state.
-        self.currently_playing = False
-        self.channel = None
+            self.currently_playing = True
+            channel = ctx.message.channel
+
+            await ctx.send(f"The game begins!")
+            # The count starts at 0.
+            count = 0
+            # The number of successful replies in a row.
+            length = 0
+            # We need to determine what the step is.
+            # It will be the first decimal number sent in the same channel.
+
+            def check_dec(m):
+                return m.channel == channel and is_decimal(m.content)
+
+            msg = await self.bot.wait_for("message", check=check)
+            # Set the step.
+            await msg.add_reaction("✅")
+            step = Decimal(msg.content)
+            length += 1
+            count += step
+
+            while True:
+                # Wait for the next numeric message
+                msg = await self.bot.wait_for("message", check=check_dec)
+                value = Decimal(msg.content)
+                if value == count + step:
+                    # If the number is correct, increase the count and length.
+                    count += step
+                    length += 1
+                    await msg.add_reaction("✅")
+                else:
+                    # Otherwise, break the chain.
+                    await msg.add_reaction("❌")
+                    await ctx.send(
+                        f"Gone wrong at {count}! The next number was {count + step}.\n"
+                        f"This chain lasted {length} consecutive messages."
+                    )
+                    break
+
+            # Reset the cog's state.
+            self.currently_playing = False
+            self.channel = None
 
 
 def setup(bot: Bot):
