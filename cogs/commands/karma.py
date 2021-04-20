fcb018b3cfec9c8251e5f3dfb8fe9b452ea764db
Jlobblet <jlobblet-github@gmx.co.uk>
2021-04-20 13:32:36 +0100

    Small formatting change

--- cogs/commands/karma.py
+++ cogs/commands/karma.py
@@ -31,7 +31,8 @@ from pytz import timezone, utc
 from sqlalchemy import func
 
 from config import CONFIG
-from models import Karma as KarmaModel, KarmaChange, db_session
+from models import Karma as KarmaModel
+from models import KarmaChange, db_session
 from utils import get_name_string, pluralise
 
 matplotlib.use("Agg")
