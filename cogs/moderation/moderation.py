15ae7beb370c57864ea3fd6f0e0cb6c30b48ab63
Jlobblet <jlobblet-github@gmx.co.uk>
2021-05-09 18:20:48 +0100

    Add unban command and handling for failing to kick, ban, or unban

--- cogs/moderation/moderation.py
+++ cogs/moderation/moderation.py
@@ -1,10 +1,11 @@
 from textwrap import dedent
 from typing import Optional
 
-from discord import Member
+from discord import HTTPException, Member, User
 from discord.ext.commands import Bot, Cog, Context, Greedy, check, command
 from discord.utils import get
 
+from config import CONFIG
 from utils import AdminError, format_list, is_compsoc_exec_in_guild
 
 
@@ -55,57 +56,138 @@ class Moderation(Cog):
             await ctx.message.add_reaction(self.emoji["what"])
             return
 
-        mentions = format_list([member.mention for member in members])
-        messages_deleted = (
-            "No messages were deleted."
-            if delete_days == 0
-            else f"Messages sent in the last {delete_days} day{'s' if delete_days != 1 else ''} were deleted."
-        )
-        with_reason = (
-            "with no reason given."
-            if reason is None
-            else f"with the reason\n> {reason}"
-        )
-        were = "were" if len(members) > 1 else "was"
+        banned = []
+        failed = []
 
         for member in members:
-            member.ban(reason=reason, delete_message_days=delete_days)
+            try:
+                await member.ban(reason=reason, delete_message_days=delete_days)
+                banned.append(member)
+            except HTTPException:
+                logging.error(f"Failed to ban {member}")
+                failed.append(member)
 
-        await ctx.send(
-            dedent(
-                f"""
+        message_parts = []
+
+        if len(banned) > 0:
+            mentions = format_list([member.mention for member in banned])
+            were = "were" if len(banned) > 1 else "was"
+            with_reason = (
+                "with no reason given."
+                if reason is None
+                else f"with the reason\n> {reason}"
+            )
+            messages_deleted = (
+                "No messages were deleted."
+                if delete_days == 0
+                else f"Messages sent in the last {delete_days} day{'s' if delete_days != 1 else ''} were deleted."
+            )
+            message_parts.append(
+                dedent(
+                    """
                 :hammer: **BANNED** :hammer:
                 {mentions} {were} banned {with_reason}
                 {messages_deleted}
                 """
+                ).format(
+                    mentions=mentions,
+                    were=were,
+                    with_reason=with_reason,
+                    messages_deleted=messages_deleted,
+                )
             )
-        )
+
+        if len(failed) > 0:
+            mentions = format_list([member.mention for member in failed])
+            message_parts.append(f"I failed to kick {mentions}")
+
+        await ctx.send("\n".join(message_parts))
+
+    @command()
+    async def unban(self, ctx: Context, users: Greedy[User], *, reason: Optional[str]):
+        if len(users) == 0:
+            await ctx.message.add_reaction(self.emoji["what"])
+
+        unbanned = []
+        failed = []
+
+        guild = self.bot.get_guild(CONFIG.UWCS_DISCORD_ID)
+        for user in users:
+            try:
+                await guild.unban(user, reason=reason)
+                unbanned.append(user)
+            except HTTPException:
+                failed.append(user)
+
+        message_parts = []
+
+        if len(unbanned) > 0:
+            mentions = format_list([str(user) for user in unbanned])
+            with_reason = (
+                "with no reason given"
+                if reason is None
+                else f"with the reason \n> {reason}"
+            )
+            were = "were" if len(unbanned) > 1 else "was"
+
+            message_parts.append(
+                dedent(
+                    """
+                :dove: **UNBANNED** :dove:
+                {mentions} {were} unbanned {with_reason}
+                """
+                ).format(mentions=mentions, were=were, with_reason=with_reason)
+            )
+
+        if len(failed) > 0:
+            mentions = format_list([str(user) for user in unbanned])
+            message_parts.append(f"I failed to unban {mentions}")
+
+        await ctx.send("\n".join(message_parts))
 
     @command()
     @only_mentions_users(True)
-    async def kick(self, ctx: Context, members: Greedy[Member], *, reason: Optional[str]):
+    async def kick(
+        self, ctx: Context, members: Greedy[Member], *, reason: Optional[str]
+    ):
         if len(members) == 0:
             await ctx.message.add_reaction(self.emoji["what"])
 
-        mentions = format([member.mention for member in members])
-        with_reason = (
-            "with no reason given"
-            if reason is None
-            else f"with the reason \n> {reason}"
-        )
-        were = "were" if len(members) > 1 else "was"
+        kicked = []
+        failed = []
 
         for member in members:
-            member.kick(reason=reason)
+            try:
+                await member.kick(reason=reason)
+                kicked.append(member)
+            except HTTPException:
+                failed.append(member)
 
-        await ctx.send(
-            dedent(
-                f"""
-                :leg: **KICKED** :leg:
-                {mentions} {were} kicked {with_reason}
-                """
+        message_parts = []
+
+        if len(kicked) > 0:
+            mentions = format_list([member.mention for member in kicked])
+            with_reason = (
+                "with no reason given"
+                if reason is None
+                else f"with the reason \n> {reason}"
             )
-        )
+            were = "were" if len(kicked) > 1 else "was"
+
+            message_parts.append(
+                dedent(
+                    """
+    :door: **KICKED** :door:
+    {mentions} {were} kicked {with_reason}
+    """
+                ).format(mentions=mentions, were=were, with_reason=with_reason)
+            )
+
+        if len(failed) > 0:
+            mentions = format_list([member.mention for member in failed])
+            message_parts.append(f"I failed to kick {mentions}")
+
+        await ctx.send("\n".join(message_parts))
 
 
 def setup(bot: Bot):
