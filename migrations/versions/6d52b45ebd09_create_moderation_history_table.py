8029bd8d18e25feb3dc3fbb2d0c36b2eddf86e2c
Jlobblet <jlobblet-github@gmx.co.uk>
2021-05-09 19:51:19 +0100

    Create moderation history table

---
+++ migrations/versions/6d52b45ebd09_create_moderation_history_table.py
@@ -0,0 +1,49 @@
+"""Create moderation history table
+
+Revision ID: 6d52b45ebd09
+Revises: f1e50ee892b4
+Create Date: 2021-05-09 19:11:38.752606
+
+"""
+import enum
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "6d52b45ebd09"
+down_revision = "f1e50ee892b4"
+branch_labels = None
+depends_on = None
+
+
+@enum.unique
+class ModerationAction(enum.Enum):
+    """Enum at the time the migration was written"""
+    TEMPMUTE = 0
+    MUTE = 1
+    UNMUTE = 2
+    WARN = 3
+    REMOVE_WARN = 4
+    AUTOWARN = 5
+    REMOVE_AUTOWARN = 6
+    KICK = 7
+    TEMPBAN = 8
+    BAN = 9
+    UNBAN = 10
+
+
+def upgrade():
+    op.create_table(
+        "moderation_history",
+        sa.Column("id", sa.Integer, primary_key=True, nullable=False),
+        sa.Column("user_id", sa.Integer, sa.ForeignKey("users.id"), nullable=False),
+        sa.Column("timestamp", sa.DateTime, nullable=False, default=sa.func.current_timestamp()),
+        sa.Column("action", sa.Enum(ModerationAction), nullable=False),
+        sa.Column("reason", sa.String, nullable=True),
+        sa.Column("moderator_id", sa.Integer, sa.ForeignKey("users.id"), nullable=False),
+    )
+
+
+def downgrade():
+    op.drop_table("moderation_history")
---
+++ models/moderation.py
@@ -0,0 +1,36 @@
+import enum
+
+from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer, String, func
+
+from models import Base, auto_str
+
+
+@enum.unique
+class ModerationAction(enum.Enum):
+    """Which moderation action a particular row represents.
+
+    Values are specified to ensure they always match up with the database.
+    """
+    TEMPMUTE = 0
+    MUTE = 1
+    UNMUTE = 2
+    WARN = 3
+    REMOVE_WARN = 4
+    AUTOWARN = 5
+    REMOVE_AUTOWARN = 6
+    KICK = 7
+    TEMPBAN = 8
+    BAN = 9
+    UNBAN = 10
+
+
+@auto_str
+class ModerationHistory(Base):
+    __tablename__ = "moderation_history"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    timestamp = Column(DateTime, nullable=False, default=func.current_timestamp())
+    action = Column(Enum(ModerationAction), nullable=False)
+    reason = Column(String, nullable=True)
+    moderator_id = Column(Integer, ForeignKey("users.id"), nullable=False)
