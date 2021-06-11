c77d3a4a6940a0e6529078cc831a49679fb25bf1
distributive <github@ameliewd.com>
2021-06-11 22:45:06 +0100

    Added missing exceptions

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,4 +1,21 @@
-class OutputTooLargeError(Exception):
+class WarningError(Exception):
+    """Raised when a user request is flagged as potentially malicious"""
+
+    """Should define __init__(self, out: str, message: str), where out is the user-facing reply"""
+
+
+class OutputTooLargeError(WarningError):
     """Raised when a message is too long to send to Discord"""
 
-    pass
+    def __init__(
+        self,
+        out="Your requested output was too large!",
+        message="Maximum Discord message length exceeded",
+    ):
+        self.out = out
+        self.message = message
+        super().__init__(self.message)
+
+
+class InternalError(Exception):
+    """Raised when something has gone wrong with the source code"""
