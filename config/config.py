c58cd4373b132231182e37542710b7844a231f99
Jlobblet <jlobblet-github@gmx.co.uk>
2021-07-12 18:34:51 +0100

    Remove moderation features

--- config/config.py
+++ config/config.py
@@ -13,17 +13,6 @@ class Config:
         self.UWCS_EXEC_ROLE_IDS: list[int] = parsed.get("UWCS_exec_role_ids")
         self.UWCS_WELCOME_CHANNEL_ID: int = parsed.get("UWCS_welcome_channel_id")
         self.UWCS_INTROS_CHANNEL_ID: int = parsed.get("UWCS_intros_channel_id")
-        self.UWCS_AUTOMOD_LOG_CHANNEL_ID: int = parsed.get(
-            "UWCS_automod_log_channel_id"
-        )
-        self.UWCS_JOIN_LEAVE_LOG_CHANNEL_ID: int = parsed.get(
-            "UWCS_join_leave_log_channel_id"
-        )
-        self.UWCS_MEMBER_LOG_CHANNEL_ID: int = parsed.get("UWCS_member_log_channel_id")
-        self.UWCS_MESSAGE_LOG_CHANNEL_ID: int = parsed.get(
-            "UWCS_message_log_channel_id"
-        )
-        self.UWCS_VOICE_LOG_CHANNEL_ID: int = parsed.get("UWCS_voice_log_channel_id")
         self.DATABASE_CONNECTION: str = parsed.get("database_connection")
         self.UWCS_DISCORD_ID: int = parsed.get("UWCS_discord_id")
         self.UWCS_API_TOKEN: str = parsed.get("UWCS_api_token")
