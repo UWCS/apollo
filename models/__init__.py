7307f83a0bd6250b0c261a3e3d6492269ceea49d
Ryan Murray <ryanrmurray@hotmail.co.uk>
2021-08-26 15:37:46 +0100

    formatting again.

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,8 @@ from models.counting import *
 from models.karma import *
 from models.messages import *
 from models.models import *
+from models.printing import *
 from models.quote import *
 from models.quoteoptouts import *
-from models.printing import *
 from models.reminder import *
 from models.user import *
