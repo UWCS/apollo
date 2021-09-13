2ebb733a00c15906fd255701c5981bc6c14ce296
Ryan Murray <43875947+RyanRMurray@users.noreply.github.com>
2021-09-13 22:35:09 +0100

    removed pointless if statement
    wow how did i miss this.

--- cogs/commands/quotes.py
+++ cogs/commands/quotes.py
@@ -300,10 +300,7 @@ class QueryInputConverter(Converter):
         if argument[0] == "@":
             return Mention.string_mention(argument[1:])
 
-        argument = await MC.convert(ctx, argument)
-
-        if argument.is_id_type():
-            return argument
+        argument = await MC.convert(ctx, argument
 
         return argument
 
