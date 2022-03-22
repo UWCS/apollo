b05214f8fb684095c04b7c0ce02ade0ff79ba11f
ericthelemur <owenconnors1@gmail.com>
2022-03-22 16:41:59 +0000

    Basic vote counting working

--- cogs/commands/vote.py
+++ cogs/commands/vote.py
@@ -10,10 +10,12 @@ from discord.ext.commands import Bot, Context, clean_content, CommandInvokeError
 from discord.ext.commands.errors import DiscordException, CommandError
 from sqlalchemy.exc import SQLAlchemyError
 
+from models import DiscordVote, db_session
 from utils import get_name_string
 from voting.discord_interfaces.discord_base import discord_base, Choice
 from voting.emoji_list import default_emojis
 from voting.splitutils import split_args
+from voting.utils import get_interface, vote_interfaces
 
 LONG_HELP_TEXT = """
 Allows running of various types of votes: FPTP, STV, etc. (WIP)
@@ -42,6 +44,32 @@ class Vote(commands.Cog):
         await discord_base.create_vote(ctx, choices)
 
 
+
+    @commands.Cog.listener()
+    @commands.guild_only()
+    async def on_raw_reaction_add(self, reaction: discord.RawReactionActionEvent):
+        if reaction.user_id == self.bot.user.id: return
+
+        interface, vote_msg_obj = get_interface(reaction.message_id)
+        if interface is None: return
+        await interface.react_add(
+            vote_msg_obj,
+            reaction.user_id,
+            str(reaction.emoji))
+
+    @commands.Cog.listener()
+    @commands.guild_only()
+    async def on_raw_reaction_remove(self, reaction: discord.RawReactionActionEvent):
+        if reaction.user_id == self.bot.user.id: return
+
+        interface, vote_msg_obj = get_interface(reaction.message_id)
+        if interface is None: return
+        await interface.react_remove(
+            vote_msg_obj,
+            reaction.user_id,
+            str(reaction.emoji))
+
+
     async def cog_command_error(self, ctx: Context, error):
         if isinstance(error, CommandInvokeError):
             if isinstance(error.original, sqlite3.Error): raise error.original
@@ -58,3 +86,5 @@ class Vote(commands.Cog):
 
 def setup(bot: Bot):
     bot.add_cog(Vote(bot))
+    for dci in vote_interfaces.values():
+        dci.bot = bot
--- voting/utils.py
+++ voting/utils.py
@@ -2,20 +2,17 @@ from typing import Dict
 
 from models import db_session
 from models.votes import DiscordVoteMessage, VoteType
-from voting.vote_types.base_vote import BaseVote, base_vote
-from sqlalchemy.orm.exc import NoResultFound
+from voting.discord_interfaces.discord_base import DiscordBase, discord_base
 
 
 
-vote_interfaces: Dict[VoteType, BaseVote] = {
-    VoteType.basic: base_vote,
+vote_interfaces: Dict[VoteType, DiscordBase] = {
+    VoteType.basic: discord_base,
 }
 
 
 def get_interface(msg_id):
-    try:
-        entry = db_session.query(DiscordVoteMessage)\
-            .filter(DiscordVoteMessage.message_id == msg_id).one()
-        return vote_interfaces[entry.vote.type]
-    except NoResultFound:
-        return None
+    entry = db_session.query(DiscordVoteMessage)\
+        .filter(DiscordVoteMessage.message_id == msg_id).one_or_none()
+    if entry is None: return None, None
+    return vote_interfaces[entry.vote.type], entry
