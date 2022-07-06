0b10b29d2063c4d9a33e154bc9748f61552d54c4
ericthelemur <owenconnors1@gmail.com>
2022-07-06 16:24:24 +0100

    Formatting

--- tests/test_message_split.py
+++ tests/test_message_split.py
@@ -1,4 +1,5 @@
 import pytest
+
 from utils.utils import split_into_messages
 
 SIMPLE_TEST_CASES = [
@@ -6,9 +7,22 @@ SIMPLE_TEST_CASES = [
     ("Line 1\n", ["Line 1"]),
     ("Line 1\nLine 2", ["Line 1\nLine 2"]),
     ("Line 1\n\nLine 2", ["Line 1\n_ _\nLine 2"]),
-    ("Longer line that would need to be split", ["Longer line that", "would need to be", "split"]),
+    (
+        "Longer line that would need to be split",
+        ["Longer line that", "would need to be", "split"],
+    ),
     ("followed by another line that would", ["followed by another", "line that would"]),
-    ("Longer line that would need to be split\nfollowed by another line that would need to be split", ["Longer line that", "would need to be", "split", "followed by another", "line that would need", "to be split"]),
+    (
+        "Longer line that would need to be split\nfollowed by another line that would need to be split",
+        [
+            "Longer line that",
+            "would need to be",
+            "split",
+            "followed by another",
+            "line that would need",
+            "to be split",
+        ],
+    ),
 ]
 
 
@@ -16,4 +30,3 @@ SIMPLE_TEST_CASES = [
 def test_message_split(string, expected):
     actual = split_into_messages(string, 20)
     assert actual == expected
-
