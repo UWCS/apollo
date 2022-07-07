e615e6234897feaf487abd4ab8dbc19fb1ab7236
ericthelemur <owenconnors1@gmail.com>
2022-07-07 22:28:23 +0100

    Made clear room_conv.py is a standalone script

--- resources/rooms/source/room_conv.py
+++ resources/rooms/source/room_conv.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 Manually run. Combines Tabula's mapping and Timetable reports to form single room mapping
 These data sources need manually fetching, so combining them is a one time thing.
@@ -18,26 +20,31 @@ custom_names = read_mapping("custom-maptotab.txt")
 custom_tabtoname = read_mapping("custom-tabtosname.txt")
 
 
-print("Missing Conversions")
-mapping = {}
-for tab, n in (tabtonames | custom_names).items():
-    if tab in custom_tabtoname:
-        name = custom_tabtoname[tab]
-    else:
-        name = n
-    url = nametourl.get(name)
-    if url is None:
-        url = nametourl.get(tab)
-    if url is None:
-        print(tab, "|", name, "|", url)
-    else:
-        mapping[tab] = url
+def main():
+    print("Missing Conversions")
+    mapping = {}
+    for tab, n in (tabtonames | custom_names).items():
+        if tab in custom_tabtoname:
+            name = custom_tabtoname[tab]
+        else:
+            name = n
+        url = nametourl.get(name)
+        if url is None:
+            url = nametourl.get(tab)
+        if url is None:
+            print(tab, "|", name, "|", url)
+        else:
+            mapping[tab] = url
 
-for name in nametourl:
-    if name not in mapping.keys() and name not in mapping.values():
-        v = nametourl[name]
-        mapping[name] = mapping[name] = v
+    for name in nametourl:
+        if name not in mapping.keys() and name not in mapping.values():
+            v = nametourl[name]
+            mapping[name] = mapping[name] = v
 
 
-with open("../room_to_surl.json", "w") as room_to_surl:
-    json.dump(mapping, room_to_surl, indent=4)
+    with open("../room_to_surl.json", "w") as room_to_surl:
+        json.dump(mapping, room_to_surl, indent=4)
+
+
+if __name__ == "__main__":
+    main()
