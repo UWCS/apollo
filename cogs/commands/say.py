fcdfeac047772e662a0e7b7f1f31ca35b48fa913
ericthelemur <owenconnors1@gmail.com>
2022-09-02 15:27:56 +0100

    Forbid /say from mentioning

--- cogs/commands/say.py
+++ cogs/commands/say.py
@@ -1,5 +1,5 @@
 import discord
-from discord import app_commands
+from discord import app_commands, AllowedMentions
 from discord.ext import commands
 from discord.ext.commands import Bot, Context, clean_content
 
@@ -16,7 +16,7 @@ class Say(commands.Cog):
 
     @app_commands.command(name="say", description=SHORT_HELP_TEXT)
     async def say_slash(self, int: discord.Interaction, message: str):
-        await int.response.send_message(discord.utils.escape_mentions(message))
+        await int.response.send_message(message, allowed_mentions=AllowedMentions.none())
 
     @commands.command(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
     async def say(self, ctx: Context, *message: clean_content):
