b397c2e305a8fdc45cecfb669b12fa2ce21fddf4
ericthelemur <owenconnors1@gmail.com>
2022-09-02 17:50:35 +0100

    Flip slash cmd

--- cogs/commands/flip.py
+++ cogs/commands/flip.py
@@ -1,4 +1,5 @@
 import random
+import shlex
 
 from discord.ext import commands
 from discord.ext.commands import Bot, Context, clean_content
@@ -16,9 +17,12 @@ class Flip(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.command(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
-    async def flip(self, ctx: Context, *args: clean_content):
+    @commands.hybrid_command(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
+    async def flip(self, ctx: Context, *, args: str = ""):
+        args = await clean_content().convert(ctx, args)
+        args = shlex.split(args)
         display_name = get_name_string(ctx.message)
+
         if len(args) == 1:
             await ctx.send(f"I can't flip just one item {display_name}! :confused:")
         else:
