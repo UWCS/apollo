a57b89a126581ce1bed2ed28387e93765ddbc629
ericthelemur <owenconnors1@gmail.com>
2022-09-23 01:47:47 +0100

    Votes resume on restart like role menus do

--- cogs/commands/vote.py
+++ cogs/commands/vote.py
@@ -7,6 +7,8 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from voting.discord_interfaces.discord_base import DiscordBase
 from voting.splitutils import split_args
+from models import db_session
+from models.votes import DiscordVoteMessage
 
 LONG_HELP_TEXT = """
 Allows running of various types of votes: FPTP, STV, etc. (WIP)
@@ -39,7 +41,19 @@ class Vote(commands.Cog):
         if len(choices) == len(args):
             choices = [args]
         print(choices)
-        await DiscordBase().create_vote(ctx, choices)
+        await DiscordBase(self.bot).create_vote(ctx, choices)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.wait_until_ready()
+
+        vote_msgs = db_session.query(DiscordVoteMessage).all()
+        for dvm in vote_msgs:
+            channel = self.bot.get_channel(dvm.channel_id)
+            msg = await channel.fetch_message(dvm.message_id)
+            if msg is None: continue
+
+            await msg.edit(view=DiscordBase(self.bot).recreate_view(dvm.vote_id, msg, dvm))
 
     async def cog_command_error(self, ctx: Context, error):
         if isinstance(error, CommandInvokeError):
