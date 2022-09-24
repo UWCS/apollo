fcc0f78fe8e71bf270d45082c2ca4888cb031a20
ericthelemur <owenconnors1@gmail.com>
2022-09-24 16:55:42 +0100

    Refactor a bit more

--- cogs/commands/event_sync.py
+++ cogs/commands/event_sync.py
@@ -38,40 +38,44 @@ class Sync(commands.Cog):
         # Add events
         for ev in cal.walk():
             if ev.name != "VEVENT":
-                continue
-
-            event_args = self.ical_event_to_dc_args(ev)
-
-            # Check for existing 
-            uid = ev.get("uid")
-            link = links.get(uid)
-
-            # Attempt to find existing event
-            event = None
-            if link:
-                event = dc_events.get(link.discord_event)
+                    continue
+            await self.update_event(ctx, ev, links, dc_events)
             
-            # If doesn't exist create a new event
-            if not event:
-                # Create new event
-                event = await ctx.guild.create_scheduled_event(**event_args)
-                await ctx.send(f"Created event **{ev.get('summary')}** at event {event.url}")
-            else:
-                # Don't edit if no change
-                if self.check_event_equiv(event_args, event):
-                    # Updat existing event
-                    await event.edit(**event_args)
-                    await ctx.send(f"Updated event **{ev.get('summary')}** at event {event.url}")
-                else:
-                    # Don't change event
-                    await ctx.send(f"No change for event **{ev.get('summary')}**")
 
-            self.update_db_link(uid, event.id, link)
+    async def update_event(self, ctx, ev, links, dc_events):
+        """Check discord events for match, update if existing, otherwise create it"""
+        event_args = self.ical_event_to_dc_args(ev)
+
+        # Check for existing 
+        uid = ev.get("uid")
+        link = links.get(uid)
+
+        # Attempt to find existing event
+        event = None
+        if link:
+            event = dc_events.get(link.discord_event)
+        
+        # If doesn't exist create a new event
+        if not event:
+            # Create new event
+            event = await ctx.guild.create_scheduled_event(**event_args)
+            await ctx.send(f"Created event **{ev.get('summary')}** at event {event.url}")
+        else:
+            # Don't edit if no change
+            if self.check_event_equiv(event_args, event):
+                # Updat existing event
+                await event.edit(**event_args)
+                await ctx.send(f"Updated event **{ev.get('summary')}** at event {event.url}")
+            else:
+                # Don't change event
+                await ctx.send(f"No change for event **{ev.get('summary')}**")
+
+        self.update_db_link(uid, event.id, link)
 
     
     @staticmethod
     def ical_event_to_dc_args(ev):
-    # Construct args for discord event
+        """Construct args for discord event from the ical event"""
         desc = f"{ev.get('description')}\n\nSee more at {ev.get('url')}"
         return {
             "name": str(ev.get("summary")),
@@ -84,12 +88,16 @@ class Sync(commands.Cog):
 
     @staticmethod
     def check_event_equiv(event_args, event):
+        """
+        Checks if there has been a change to the event. 
+        Slight faff since we don't want to compare all fields of the discord event or the ical event
+        """
         old_args = {"name": event.name, "description": event.description, "start_time": event.start_time, "end_time": event.end_time, "entity_type": event.entity_type, "location": event.location}
         return event_args != old_args
 
     @staticmethod
     def update_db_link(uid, event_id, link):
-        # Update database record
+        """Update database record. If new, create, otherwise update"""
         now = datetime.datetime.now()
         if not link:
             link = EventLink(uid=uid, discord_event=event_id, last_modified=now)
