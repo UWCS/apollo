f6636eb80d1f301466ae60609b52fef277f9e96d
Karl <58265182+KarlKennerley@users.noreply.github.com>
2022-12-31 22:15:35 +0000

    Feature/Extended flip for option weights.
    Closes issue #139.
    
    Added the ability to add weights to provided options passed as arguments. The keyword "weights" separates the options and weights in the supplied arguments. The number of options must match the number of weights.

--- cogs/commands/flip.py
+++ cogs/commands/flip.py
@@ -7,7 +7,7 @@ from discord.ext.commands import Bot, Context, clean_content
 from utils import get_name_string
 
 LONG_HELP_TEXT = """
-Picks randomly between two options (or heads and tails if left blank)
+Picks randomly between two (potentially weighted) options (or heads and tails if left blank).
 """
 
 SHORT_HELP_TEXT = """Picks randomly between two options"""
@@ -22,13 +22,23 @@ class Flip(commands.Cog):
         args = await clean_content().convert(ctx, args)
         args = shlex.split(args)
         display_name = get_name_string(ctx.message)
+        weighted = False
 
         if len(args) == 1:
             await ctx.send(f"I can't flip just one item {display_name}! :confused:")
+        
         else:
-            options = ["Heads", "Tails"] if not args else args
-
-            await ctx.send(f'{display_name}: {random.choice(options).lstrip("@")}')
+            if "weights" in [arg.lower() for arg in args]:
+                if len(args[:args.index("weights")]) == len(args[:args.index("weights")+1:])):
+                    options = args[:args.index("weights")]
+                    option_weights = args[args.index("weights")+1:]
+                    weighted = True
+                else:
+                    await ctx.send(f"The number of options and weights supplied must match {display_name}! :confused:")
+            else:
+                options = ["Heads", "Tails"] if not args else args
+             
+            await ctx.send(f"{display_name}: ({random.choices(options, weights=option_weights, k=1)[0] if weighted else {random.choice(options)).lstrip("@")}")
 
 
 async def setup(bot: Bot):
