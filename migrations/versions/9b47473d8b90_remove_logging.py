8fd879b3babb3479370d6e3d41314f62459f61f1
ericthelemur <owenconnors1@gmail.com>
2023-01-06 15:19:40 +0000

    Guess downgrade and format

--- migrations/versions/9b47473d8b90_remove_logging.py
+++ migrations/versions/9b47473d8b90_remove_logging.py
@@ -8,11 +8,11 @@ Create Date: 2022-12-31 15:45:53.589887
 import sqlalchemy as sa
 import sqlalchemy_utils as sau
 from alembic import op
-from config.config import CONFIG
+from sqlalchemy.ext.declarative import declarative_base
 
+from config.config import CONFIG
 from models.karma import KarmaChange
 from models.models import auto_str
-from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -25,8 +25,9 @@ depends_on = None
 # https://alembic.sqlalchemy.org/en/latest/batch.html#dropping-unnamed-or-named-foreign-key-constraints
 naming_convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "%(table_name)s_pkey"
+    "pk": "%(table_name)s_pkey",
 }
+
 
 @auto_str
 class KarmaChange(Base):
@@ -40,6 +41,7 @@ class KarmaChange(Base):
     reason = sa.Column(sa.String(), nullable=True)
     change = sa.Column(sa.Integer, nullable=False)
     score = sa.Column(sa.Integer, nullable=False)
+
 
 @auto_str
 class LoggedMessage(Base):
@@ -57,32 +59,44 @@ class LoggedMessage(Base):
         sau.EncryptedType(type_in=sa.String, key=CONFIG.BOT_SECRET_KEY), nullable=False
     )
 
+
 def upgrade():
     bind = op.get_bind()
     session = sa.orm.Session(bind=bind)
 
     # Drop FK and add column
-    with op.batch_alter_table("karma_changes", recreate="always", naming_convention=naming_convention) as batch_op:
-        if bind.engine.name != "sqlite": 
+    with op.batch_alter_table(
+        "karma_changes", recreate="always", naming_convention=naming_convention
+    ) as batch_op:
+        if bind.engine.name != "sqlite":
             op.drop_constraint("karma_changes_message_id_fkey", "karma_changes")
-        batch_op.add_column(sa.Column("mid_new", sa.BigInteger, server_default="-1"), insert_after="message_id")
+        batch_op.add_column(
+            sa.Column("mid_new", sa.BigInteger, server_default="-1"),
+            insert_after="message_id",
+        )
 
     # Update value of new column
     for change in session.query(KarmaChange):
-        msg = session.query(LoggedMessage).where(LoggedMessage.id == change.message_id).first()
+        msg = (
+            session.query(LoggedMessage)
+            .where(LoggedMessage.id == change.message_id)
+            .first()
+        )
         change.mid_new = msg.message_uid
     session.commit()
 
     # Edit PK and column names
-    with op.batch_alter_table("karma_changes", schema=None, naming_convention=naming_convention) as bop:
+    with op.batch_alter_table(
+        "karma_changes", schema=None, naming_convention=naming_convention
+    ) as bop:
         # bop.drop_constraint("pk_karma_changes", type_='primary')
-        bop.drop_constraint("karma_changes_pkey", type_='primary')
-        bop.create_primary_key('pk_karma_changes', ['karma_id', 'user_id', 'mid_new'])
+        bop.drop_constraint("karma_changes_pkey", type_="primary")
+        bop.create_primary_key("pk_karma_changes", ["karma_id", "user_id", "mid_new"])
         bop.alter_column("message_id", new_column_name="mid_old", nullable=True)
         bop.alter_column("mid_new", new_column_name="message_id", server_default=None)
 
     session.commit()
-    
+
     # op.drop_table("message_edits")
     # op.drop_table("messages")
 
@@ -98,7 +112,6 @@ def downgrade():
 
     with op.batch_alter_table("karma_changes") as batch_op:
         batch_op.drop_column("mid_old")
-
 
     with op.batch_alter_table("karma_changes") as batch_op:
         batch_op.create_foreign_key(
