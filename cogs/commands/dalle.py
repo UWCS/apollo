72515c342a5551cc21c8ef2b8d28e0cc7694c7bd
Joel Coulon <Joel.Coulon@warwick.ac.uk>
2023-04-01 22:38:26 +0100

    isort and black applied fixed typos removed uncessary cleaninf removed unused variables added general comments

--- cogs/commands/dalle.py
+++ cogs/commands/dalle.py
@@ -1,30 +1,22 @@
 import logging
+from io import BytesIO
+
+import aiohttp
 import discord
 import openai
 from discord.ext import commands
 from discord.ext.commands import Bot, Context, clean_content
-import aiohttp
-import asyncio
-from io import BytesIO
 
 from config import CONFIG
 
 LONG_HELP_TEXT = """
 Apollo is more creative than you think...
 
-Apollo can now generate imagefes using openAI's DALL-E model. 
-To use Apollo, simply type `!dalle <prompt>` . Apollo will then generate an image based on the prompt.
+Apollo can now generate images using openAI's DALL-E model. 
+To use, simply type `!dalle <prompt>` . Apollo will then generate an image based on the prompt.
 """
 
 SHORT_HELP_TEXT = "Apollo is more creative than you think..."
-
-chat_cmd = CONFIG.PREFIX + "dalle"
-
-def clean(msg, *prefixes):
-    for pre in prefixes:
-        msg = msg.strip().removeprefix(pre)
-    return msg.strip()
-
 
 class Dalle(commands.Cog):
     def __init__(self, bot: Bot):
@@ -32,27 +24,29 @@ class Dalle(commands.Cog):
         openai.api_key = CONFIG.OPENAI_API_KEY
         self.cooldowns = {}
 
-    @commands.command()
-    async def dalle(self, ctx: Context, *, prompt: clean_content):
-        prompt = clean(prompt, chat_cmd, CONFIG.PREFIX)
+    @commands.hybrid_command(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
+    async def dalle(self, ctx: Context, *, args: str = ""):
+        """Generates an image based on the prompt using DALL-E"""
+        prompt = await clean_content().convert(ctx, args)
         url = self.generate_image(prompt)
         image = self.get_image(url)
         if image is not None:
-           await ctx.reply(file=image, mention_author=True)
+            await ctx.reply(file=image, mention_author=True)
         else:
-            await ctx.reply("Failed to generate image", mention_author=True)
-        
+            await ctx.reply("Failed to generate image :wah:", mention_author=True)
 
-    async def generate_image(self, prompt): # generates the image from open ai
+    async def generate_image(self, prompt):
+        """gets image from openAI and returns url for that image"""
         logging.info(f"Generating image with prompt: {prompt}")
         response = await openai.Image.acreate(
             prompt=prompt,
             n=1,
-            size="256x256",
+            size="256x256",  # maybe change later? (you're wlecome treasurer btw)
         )
-        return response["data"][0]["url"] # returns the url of the image
-    
+        return response["data"][0]["url"]
+
     async def get_image(self, url):
+        """gets image from url and returns it as a discord file"""
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 if response.status == 200:
