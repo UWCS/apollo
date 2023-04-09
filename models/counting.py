0e4a159993bb8aef07c285de7e1a7ca61bb454a7
Joey Harrison <joey@joeyh.dev>
2023-04-09 18:52:57 +0000

    migrate abt half to dataclasses

--- models/counting.py
+++ models/counting.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from decimal import Decimal
 
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, mapped_column
 
 from models.models import Base, int_pk, user_id
 
@@ -9,7 +9,7 @@ from models.models import Base, int_pk, user_id
 class CountingRun(Base):
     __tablename__ = "counting_runs"
 
-    id: Mapped[int_pk]
+    id: Mapped[int_pk] = mapped_column(init=False)
     started_at: Mapped[datetime]
     ended_at: Mapped[datetime]
     length: Mapped[int]
@@ -18,7 +18,7 @@ class CountingRun(Base):
 
 class CountingUser(Base):
     __tablename__ = "counting_users"
-    id: Mapped[int_pk]
+    id: Mapped[int_pk] = mapped_column(init=False)
 
     user_id: Mapped[user_id]
     correct_replies: Mapped[int]
--- models/models.py
+++ models/models.py
@@ -1,9 +1,8 @@
 import logging
 
 from sqlalchemy import BigInteger, ForeignKey, create_engine
-from sqlalchemy.orm import DeclarativeBase, Session, mapped_column
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass, Session, mapped_column
 from typing_extensions import Annotated
-
 from config import CONFIG
 
 # this is bad, redo this
@@ -13,32 +12,15 @@ if CONFIG.SQL_LOGGING:
     logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
 db_session = Session(bind=engine, future=True)
 
-
-# also to be deprecated in favour of new dataclass functionality
-def auto_str(cls):
-    def __repr__(self):
-        value = ", ".join(
-            "{}={}".format(*item)
-            for item in vars(self).items()
-            if item[0] != "_sa_instance_state"
-        )
-        return f"{type(self).__name__} {{{value}}}"
-
-    cls.__repr__ = __repr__
-    return cls
-
-
-int_pk = Annotated[int, mapped_column(primary_key=True)]
+# some useful re-usable column types
+int_pk = Annotated[int, mapped_column(primary_key=True, init=False)]
 user_id = Annotated[int, mapped_column(ForeignKey("users.id"), nullable=False)]
-
 # discord 'snowflakes' are the really long IDs that you get as like channel or user IDs
 # this annotated column map type uses BigInteger to encode those and convert to python ints
 discord_snowflake = Annotated[int, mapped_column(BigInteger, nullable=False)]
-discord_snowflake_pk = Annotated[int, mapped_column(BigInteger, primary_key=True)]
 
-# TODO: add MappedAsDataclass as subclass
-# some more tidying required first...
-class Base(DeclarativeBase):
+
+class Base(MappedAsDataclass, DeclarativeBase):
     """
     Base model for all of Apollo's Models
     Uses SQLAlchemy's declarative dataclass mapping API
