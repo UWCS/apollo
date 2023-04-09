12970debf9c5d51cbf26ac64bf0e3dc9a73919e5
Joey Harrison <joey@joeyh.dev>
2023-04-09 22:07:44 +0000

    tests now passing

--- models/quote.py
+++ models/quote.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy import func, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from models.models import Base, user_id, int_pk
+from models.models import Base, user_id
 from models.user import User
 from utils.mentions import MentionType
 from typing import Optional
@@ -11,21 +11,20 @@ from typing import Optional
 
 class Quote(Base):
     __tablename__ = "quotes"
-    quote_id: Mapped[int_pk] = mapped_column(autoincrement=True, init=False)
+    quote_id: Mapped[int] = mapped_column(
+        primary_key=True, autoincrement=True, init=False
+    )
     author_type: Mapped[MentionType]
     quote: Mapped[str]
-    author_id: Mapped[Optional[int]] = mapped_column(
-        ForeignKey("users.id"), default=None
+    author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("users.id"))
+    author: Mapped[Optional["User"]] = relationship(
+        "User", uselist=False, foreign_keys=author_id, init=False
     )
-
     author_string: Mapped[Optional[str]] = mapped_column(default=None)
     created_at: Mapped[datetime] = mapped_column(
         default_factory=datetime.now, insert_default=func.current_timestamp()
     )
     edited_at: Mapped[Optional[datetime]] = mapped_column(default=None)
-    author: Mapped[Optional["User"]] = relationship(
-        "User", uselist=False, foreign_keys=author_id, default=None
-    )
 
     def author_to_string(self) -> str:
         match self.author_type:
@@ -52,6 +51,7 @@ class Quote(Base):
             author_string=string,
             quote=quote,
             created_at=created,
+            author_id=None,
         )
 
 
@@ -59,5 +59,5 @@ class QuoteOptouts(Base):
     __tablename__ = "quotes_opt_out"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, init=False)
     user_type: Mapped[MentionType]
-    user_id: Mapped[user_id]
+    user_id: Mapped[Optional[int]] = mapped_column(ForeignKey("users.id"))
     user_string: Mapped[Optional[str]]
