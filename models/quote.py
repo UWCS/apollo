c21ca8e82303ae019dceb65c791aaa9a41a78fcc
Joey Harrison <joey@joeyh.dev>
2023-04-09 00:45:40 +0000

    quotes done

--- models/quote.py
+++ models/quote.py
@@ -1,36 +1,41 @@
-from sqlalchemy import Column, DateTime, Enum, ForeignKey, Integer, String, Text, func
-from sqlalchemy.orm import relationship
+from sqlalchemy import func
+from sqlalchemy.orm import relationship, mapped_column, Mapped
 
-from models.models import Base, auto_str
+from models.models import Base, user_id
 from utils.mentions import MentionType
+from datetime import datetime
+from models.user import User
 
 
-@auto_str
 class Quote(Base):
     __tablename__ = "quotes"
-    quote_id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    author_type = Column(Enum(MentionType), nullable=False)
-    author_id = Column(Integer, ForeignKey("users.id"), nullable=True)
-    author_string = Column(String, nullable=True)
-    quote = Column(Text, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=func.current_timestamp())
-    edited_at = Column(DateTime, nullable=True)
+    quote_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    author_type: Mapped[MentionType]
+    author_id: Mapped[user_id | None]
+    author_string: Mapped[str | None]
 
-    author = relationship("User", uselist=False, foreign_keys=author_id)
+    quote: Mapped[str]
+    created_at: Mapped[datetime] = mapped_column(default=func.current_timestamp())
+    edited_at: Mapped[datetime | None]
+
+    author: Mapped["User"] = relationship("User", uselist=False, foreign_keys=author_id)  # type: ignore # there is probably a better way to do this
 
     def author_to_string(self) -> str:
-        if self.author_type == MentionType.ID:
-            return f"<@{self.author.user_uid}>"
-        return self.author_string
+        match self.author_type:
+            case MentionType.ID:
+                return f"<@{self.author.user_uid}>"
+            case MentionType.STRING:
+                assert self.author_string
+                return self.author_string
 
     @staticmethod
-    def id_quote(id, quote, created):
+    def id_quote(id: int, quote: str, created: datetime):
         return Quote(
             author_type=MentionType.ID, author_id=id, quote=quote, created_at=created
         )
 
     @staticmethod
-    def string_quote(string, quote, created):
+    def string_quote(string: str, quote: str, created: datetime):
         return Quote(
             author_type=MentionType.STRING,
             author_string=string,
@@ -39,10 +44,9 @@ class Quote(Base):
         )
 
 
-@auto_str
 class QuoteOptouts(Base):
     __tablename__ = "quotes_opt_out"
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    user_type = Column(Enum(MentionType), nullable=False)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
-    user_string = Column(String, nullable=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_type: Mapped[MentionType]
+    user_id: Mapped[user_id]
+    user_string: Mapped[str]
