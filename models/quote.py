4b5239a1d262fb718dac78f0179068ddfd4772b0
Joey Harrison <joey@joeyh.dev>
2023-04-09 19:02:19 +0000

    migrate more to dataclasses

--- models/quote.py
+++ models/quote.py
@@ -3,27 +3,30 @@ from datetime import datetime
 from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from models.models import Base, user_id
+from models.models import Base, user_id, int_pk
 from models.user import User
 from utils.mentions import MentionType
 
 
 class Quote(Base):
     __tablename__ = "quotes"
-    quote_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    quote_id: Mapped[int_pk] = mapped_column(autoincrement=True, init=False)
     author_type: Mapped[MentionType]
-    author_id: Mapped[user_id | None]
-    author_string: Mapped[str | None]
-
     quote: Mapped[str]
-    created_at: Mapped[datetime] = mapped_column(default=func.current_timestamp())
-    edited_at: Mapped[datetime | None]
-
-    author: Mapped["User"] = relationship("User", uselist=False, foreign_keys=author_id)  # type: ignore # there is probably a better way to do this
+    author_id: Mapped[user_id | None] = mapped_column(default=None)
+    author_string: Mapped[str | None] = mapped_column(default=None)
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.now(), insert_default=func.current_timestamp()
+    )
+    edited_at: Mapped[datetime | None] = mapped_column(default=None)
+    author: Mapped["User" | None] = relationship(
+        "User", uselist=False, foreign_keys=author_id, default=None
+    )
 
     def author_to_string(self) -> str:
         match self.author_type:
             case MentionType.ID:
+                assert self.author
                 return f"<@{self.author.user_uid}>"
             case MentionType.STRING:
                 assert self.author_string
@@ -32,7 +35,10 @@ class Quote(Base):
     @staticmethod
     def id_quote(id: int, quote: str, created: datetime):
         return Quote(
-            author_type=MentionType.ID, author_id=id, quote=quote, created_at=created
+            author_type=MentionType.ID,
+            author_id=id,
+            quote=quote,
+            created_at=created,
         )
 
     @staticmethod
@@ -47,7 +53,7 @@ class Quote(Base):
 
 class QuoteOptouts(Base):
     __tablename__ = "quotes_opt_out"
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, init=False)
     user_type: Mapped[MentionType]
     user_id: Mapped[user_id]
     user_string: Mapped[str]
