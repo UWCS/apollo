7045bf0b86223a7e3be977839ec427bb56f416a0
Joey Harrison <joey@joeyh.dev>
2023-04-09 19:02:19 +0000

    migrate more to dataclasses

--- models/reminder.py
+++ models/reminder.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from models.models import Base, discord_snowflake, int_pk, user_id, dat
+from models.models import Base, discord_snowflake, int_pk, user_id
 from models.user import User
 
 
@@ -12,13 +12,14 @@ class Reminder(Base):
     id: Mapped[int_pk] = mapped_column(init=False)
     user_id: Mapped[user_id]
     reminder_content: Mapped[str]
-    created_at: Mapped[datetime_default_now] = mapped_column(default=datetime.now())
     trigger_at: Mapped[datetime]
     triggered: Mapped[bool]
     playback_channel_id: Mapped[discord_snowflake]
-    irc_name: Mapped[str | None] = mapped_column(default=None)
-
     user: Mapped["User"] = relationship(
         "User",
         uselist=False,
+    )
+    irc_name: Mapped[str | None] = mapped_column(default=None)
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.now(), insert_default=func.current_timestamp()
     )
--- models/role_menu.py
+++ models/role_menu.py
@@ -6,17 +6,15 @@ from models.models import Base, discord_snowflake, int_pk
 
 class RoleMenu(Base):
     __tablename__ = "rolemenu"
-    id: Mapped[int_pk]
+    id: Mapped[int_pk] = mapped_column(init=False)
     msg_ref: Mapped[str]
     guild_id: Mapped[discord_snowflake]
-    title: Mapped[str] = mapped_column(server_default="Vote")
     channel_id: Mapped[discord_snowflake]
-    message_id: Mapped[discord_snowflake | None] = mapped_column(default=None)
-
     choices: Mapped[list["RoleEntry"]] = relationship(
         "RoleEntry", back_populates="menu", cascade="all, delete-orphan"
     )
-
+    title: Mapped[str] = mapped_column(default="Vote", insert_default="Vote")
+    message_id: Mapped[discord_snowflake | None] = mapped_column(default=None)
     unique_roles: Mapped[bool] = mapped_column(default=False)
 
 
@@ -26,10 +24,8 @@ class RoleEntry(Base):
         ForeignKey("rolemenu.id", ondelete="CASCADE"),
         primary_key=True,
     )
-
-    role: Mapped[discord_snowflake_pk]
+    menu: Mapped["RoleMenu"] = relationship(back_populates="choices")
+    role: Mapped[discord_snowflake] = mapped_column(primary_key=True)
     title: Mapped[str]
     description: Mapped[str | None] = mapped_column(default=None)
     emoji: Mapped[str | None] = mapped_column(default=None)
-
-    menu: Mapped["RoleMenu"] = relationship(back_populates="choices")
