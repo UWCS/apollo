620f7baf7633019ccab940200b7f477a51c84c6e
Joey Harrison <joey@joeyh.dev>
2023-04-10 22:09:36 +0000

    refactor and add to info command

--- cogs/commands/system.py
+++ cogs/commands/system.py
@@ -4,54 +4,79 @@ from config import CONFIG
 import aiohttp
 from os import environ
 import logging
-import sys
 import discord
 import platform
+from datetime import datetime
+from typing import Any
 
+APOLLO_URL = (
+    "https://portainer.uwcs.co.uk/api/endpoints/2/docker/containers/apollo/json"
+)
 
 class System(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.api_key = CONFIG.PORTAINER_API_KEY
+
         try:
             with open(".version", "r") as f:
-                self.version_from_file = f.read()
+                content = f.readlines()
+                self.version_from_file = content[0]
+                self.build_timestamp_from_file = content[1]
         except FileNotFoundError:
             logging.error("Could not load git revision from file")
             self.version_from_file = None
+            self.build_timestamp_from_file = None
 
     @commands.hybrid_command()
-    async def info(self, ctx: Context):
-        """Info about Apollo's build and environment"""
-        # The below works, but the labels reported by the API are often out of date
-        # for reasons I cannot figure out. TODO figure it out.
-        url = (
-            "https://portainer.uwcs.co.uk/api/endpoints/2/docker/containers/apollo/json"
-        )
-        headers = {"X-API-Key": f"{CONFIG.PORTAINER_API_KEY}"}
-        async with aiohttp.ClientSession(headers=headers) as session:
-            resp = await session.get(url)
-            if not resp.ok:
-                raise Exception("Could not reach Portainer API")
-            content = await resp.json()
+    async def info(self, ctx: Context[Bot]):
+        json = await self.get_docker_json()
+        if json is None:
+            return
 
-        version = content["Config"]["Labels"]["org.opencontainers.image.revision"]
-        built = content["Config"]["Labels"]["org.opencontainers.image.created"]
-        description = content["Config"]["Labels"][
-            "org.opencontainers.image.description"
-        ]
+        # these are likely to be outdated, so we don't use them.
+        # version = json["Config"]["Labels"]["org.opencontainers.image.revision"]
+        # built = json["Config"]["Labels"]["org.opencontainers.image.created"]
+
+        version = self.version_from_file
+        built = self.build_timestamp_from_file
+
+        description = json["Config"]["Labels"]["org.opencontainers.image.description"]
+
         py_version = platform.python_version()
         dpy_version = discord.__version__
-        reply = f"Apollo, {description}\nBuilt from Git revision {version[:8]} on {built[0:10]} {built[11:19]}\nPython {py_version}, discord.py {dpy_version} "
+        started = datetime.fromisoformat(json["State"]["StartedAt"])
+        uptime = datetime.now() - started
+
+        if version and built:
+            reply = f"""Apollo, {description}\n
+Built from Git revision {version[:8]} on {built[0:10]} {built[11:19]}
+Python {py_version}, discord.py {dpy_version} 
+Started {started} (uptime {uptime})"""
+
+        else:
+            reply = f"""Apollo, {description}\n
+Could not get build information
+Python {py_version}, discord.py {dpy_version} 
+Started {started} (uptime {uptime})"""
         await ctx.reply(reply)
 
     @commands.hybrid_command()
-    async def version(self, ctx: Context):
+    async def version(self, ctx: Context[Bot]):
         """Get Apollo's version"""
         if self.version_from_file:
             await ctx.reply(self.version_from_file)
         else:
             await ctx.reply("Could not find version")
+
+    @staticmethod
+    async def get_docker_json() -> dict[Any, Any] | None:
+        headers = {"X-API-Key": f"{CONFIG.PORTAINER_API_KEY}"}
+        async with aiohttp.ClientSession(headers=headers) as session:
+            resp = await session.get(APOLLO_URL)
+            if not resp.ok:
+                logging.error("Could not reach Portainer API")
+                return None
+            return await resp.json()
 
 
 async def setup(bot: Bot):
@@ -59,7 +84,7 @@ async def setup(bot: Bot):
     headers = {"X-API-Key": f"{CONFIG.PORTAINER_API_KEY}"}
     async with aiohttp.ClientSession(headers=headers) as session:
         resp = await session.get(url)
-    match (resp.ok, environ.get("CONTAINER") is not None):
+    match (resp.ok, (environ.get("CONTAINER") is not None)):
         case (True, True):
             await bot.add_cog(System(bot))
         case (True, False):
@@ -70,5 +95,5 @@ async def setup(bot: Bot):
             logging.error(
                 "Running in container but can't reach Portainer API. Not loading system cog."
             )
-        case (False, False):
+        case _:
             logging.error("Not loading system cog.")
