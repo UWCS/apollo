3fadf77dca5879b541b9b8c2a2a2f24fdb17d890
Joey Harrison <joey@joeyh.dev>
2023-04-12 21:58:15 +0000

    isort lol

--- migrations/env.py
+++ migrations/env.py
@@ -1,11 +1,10 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
-from models import Base
+from sqlalchemy import engine_from_config, pool
+
 from config import CONFIG
+from models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- migrations/versions/b3e10d318fa8_fresh_start.py
+++ migrations/versions/b3e10d318fa8_fresh_start.py
@@ -5,9 +5,8 @@ Revises:
 Create Date: 2023-04-12 21:50:28.949647
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "b3e10d318fa8"
