54ff9193047354b3b488ea33e5ab901592f004f8
Joey Harrison <joey@joeyh.dev>
2023-04-14 11:09:34 +0000

    fixes

--- cogs/commands/karma_admin.py
+++ cogs/commands/karma_admin.py
@@ -8,7 +8,7 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from cogs.commands.karma import current_milli_time
 from models import db_session
-from models.karma import IgnoredChannel, MiniKarmaChannel
+from models.channel_settings import IgnoredChannel, MiniKarmaChannel
 from utils import EnumGet, get_database_user, is_compsoc_exec_in_guild
 
 LONG_HELP_TEXT = """
