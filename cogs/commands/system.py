38511e50c19222c540f51eab7dea019400a33ece
Mole1424 <joel@joelcoulon.com>
2023-04-15 23:08:50 +0000

    formatting owen.....

--- cogs/commands/system.py
+++ cogs/commands/system.py
@@ -117,7 +117,9 @@ Started {started} (uptime {uptime})"""
             resp = await session.post(url)
             if not resp.ok:
                 status = resp.status
-                err_msg = f"Failed to update. {status} from Portainer API: {resp.text()}"
+                err_msg = (
+                    f"Failed to update. {status} from Portainer API: {resp.text()}"
+                )
                 logging.error(err_msg)
                 await ctx.reply(err_msg)
                 # remove our event that just failed to happen
