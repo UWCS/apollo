cf0416c323f88661818fe01e320334eacd757003
Mole1424 <joel@joelcoulon.com>
2023-05-07 20:50:37 +0000

    sligh modifcation to db added user ages

--- cogs/commands/birthday.py
+++ cogs/commands/birthday.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from discord import User
 from discord.ext import commands
 from discord.ext.commands import Bot, Context
 from pytz import timezone, utc
@@ -13,8 +14,6 @@ Hapy birthday!!!!
 
 As is tradtion, wish our dear lord chancellor of the computer a happy birthday
 """
-
-SHORT_HELP_TEXT = "Hapy birthday!!!!"
 
 
 class Birthday(commands.Cog):
@@ -30,28 +29,43 @@ class Birthday(commands.Cog):
             )
             self.age = 0
 
-    @commands.hybrid_command(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
+    @commands.hybrid_command(help=LONG_HELP_TEXT, brief="HAPPY BIRTHDAY!!!!")
     async def birthday(self, ctx: Context):
+        """Adds 1 to the age of the Lord Chancellor and wishes them a happy birthday"""
         current_date = utc.localize(datetime.now()).astimezone(
             timezone("Europe/London")
-        )
+        )  # gets the current date
         if current_date.date() <= self.date.date():
             return await ctx.reply(
-                "I'm sorry but our lord chancellor has already been wished a happy birthday today"
-            )
+                "I'm sorry but our high and gracious Lord Chancellor has already been wished a happy birthday today"
+            )  # cannot wish happy birthday twice in one day
         self.date = current_date
         self.age += 1
-        borth = db_Birthday(date=self.date, age=self.age)
+        borth = db_Birthday(date=self.date, age=self.age, user_id=ctx.author.id)
         db_session.add(borth)
         db_session.commit()
+        # update the database
         await ctx.reply(
             f"Happy birthday!!!! <@{CONFIG.LORD_CHANCELLOR_ID}>, you are now {self.age}"
         )
 
-    @commands.hybrid_command(help="get current age", brief="get current age")
+    @birthday.command(help="Get the current age of the Lord Chancellor")
     async def age(self, ctx: Context):
+        """Get the current age of the Lord Chancellor"""
         name = self.bot.get_user(CONFIG.LORD_CHANCELLOR_ID).name
         await ctx.reply(f"{name} is {self.age}")
+
+    @birthday.command(
+        help="How many times has someone wished the Lord Chancellor a happy birthday?",
+    )
+    async def user(self, ctx: Context, user: User):
+        """How many times has someone wished the Lord Chancellor a happy birthday?"""
+        num = (
+            db_session.query(db_Birthday).filter(db_Birthday.user_id == user.id).count()
+        )
+        await ctx.reply(
+            f"{user.name} has wished the Lord Chancellor a happy birthday {num} times"
+        )
 
 
 async def setup(bot: Bot):
--- models/birthday.py
+++ models/birthday.py
@@ -2,10 +2,11 @@ from datetime import datetime
 
 from sqlalchemy.orm import Mapped, mapped_column
 
-from models.models import Base
+from models.models import Base, UserId
 
 
 class Birthday(Base):
     __tablename__ = "birthdays"
     date: Mapped[datetime] = mapped_column(primary_key=True)
     age: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[UserId]
