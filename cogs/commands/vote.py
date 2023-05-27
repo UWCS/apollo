b6331423a4fbdbaba85a84d8f2175751c75a0404
ericthelemur <owenconnors1@gmail.com>
2023-05-27 13:11:40 +0100

    End votes if ancient

--- cogs/commands/vote.py
+++ cogs/commands/vote.py
@@ -50,15 +50,17 @@ class Vote(commands.Cog):
         await self.bot.wait_until_ready()
 
         vote_msgs = db_session.query(DiscordVoteMessage).all()
+        threshold_time = datetime.now().astimezone(timezone.utc) - timedelta(months=1)
         for dvm in vote_msgs:
             channel = self.bot.get_channel(dvm.channel_id)
             msg = await channel.fetch_message(dvm.message_id)
-            if msg is None:
-                continue
-
-            await msg.edit(
-                view=DiscordBase(self.bot).recreate_view(dvm.vote_id, msg, dvm)
-            )
+            if msg is None or msg.created_at < threshold_time:
+                print("Ending", msg)
+                await msg.edit(view=None)
+            else:
+                await msg.edit(
+                    view=DiscordBase(self.bot).recreate_view(dvm.vote_id, msg, dvm)
+                )
 
 
 async def setup(bot: Bot):
