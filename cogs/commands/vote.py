d7d45d1ec12959ee5ce85a8239eff9d8324782a2
ericthelemur <owenconnors1@gmail.com>
2023-05-27 01:16:35 +0100

    End votes if ancient

--- cogs/commands/vote.py
+++ cogs/commands/vote.py
@@ -21,20 +21,18 @@ class Vote(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    # @commands.hybrid_group(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
-    # async def vote(self, ctx: Context):
-    #     if not ctx.invoked_subcommand:
-    #         await ctx.send("Subcommand not found")
+    @commands.hybrid_group(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
+    async def vote(self, ctx: Context):
+        if not ctx.invoked_subcommand:
+            await ctx.send("Subcommand not found")
 
-    # @vote.command(
-    @commands.hybrid_command(
+    @vote.command(
         help="Run basic poll with given options, votes are visible. Provide options as new-line, semi-colon, comma or space separated options, the first will be taken as the title. If you wish to use a separator in an option, escape it with a backslash `\\`.",
         brief="Runs basic poll with visible votes. Can use `\\n`, `;`, `,` or ` ` separators. Escape with `\\`",
-        aliases=["poll"],
+        aliases=["quick", "visible"],
         usage="<title>; [<option 1>; <option 2>; [...]]",
     )
-    # async def basic(self, ctx: Context, *, args: str):
-    async def vote(self, ctx: Context, *, args: str):
+    async def basic(self, ctx: Context, *, args: str):
         print(args)
         args = await clean_content().convert(ctx, args)
         # noinspection PyTypeChecker
@@ -50,15 +48,17 @@ class Vote(commands.Cog):
         await self.bot.wait_until_ready()
 
         vote_msgs = db_session.query(DiscordVoteMessage).all()
+        threshold_time = datetime.now().astimezone(timezone.utc) - timedelta(months=1)
         for dvm in vote_msgs:
             channel = self.bot.get_channel(dvm.channel_id)
             msg = await channel.fetch_message(dvm.message_id)
-            if msg is None:
-                continue
-
-            await msg.edit(
-                view=DiscordBase(self.bot).recreate_view(dvm.vote_id, msg, dvm)
-            )
+            if msg is None or msg.created_at < threshold_time:
+                print("Ending", msg)
+                await msg.edit(view=None)
+            else:
+                await msg.edit(
+                    view=DiscordBase(self.bot).recreate_view(dvm.vote_id, msg, dvm)
+                )
 
 
 async def setup(bot: Bot):
