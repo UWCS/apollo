e6f2e21f8058ab66ad15710ae515c83787199ecd
Joey Harrison <joey@joeyh.dev>
2023-07-17 09:12:16 +0000

    report errors in run command

--- cogs/commands/run.py
+++ cogs/commands/run.py
@@ -94,39 +94,36 @@ class Run(commands.Cog):
 
         json_request = {"code": code, "lang": str(language.value), "input": input}
 
-        await ctx.message.add_reaction("⏳")
+        async with ctx.typing():
 
-        async with aiohttp.ClientSession() as session:
-            run_endpoint = CONFIG.PYROMANIAC_URL + "/api/run"
-            response = await session.post(run_endpoint, json=json_request)
+            async with aiohttp.ClientSession() as session:
+                run_endpoint = CONFIG.PYROMANIAC_URL + "/api/run"
+                response = await session.post(run_endpoint, json=json_request)
             if not response.ok:
-                body = await response.text()
-                logging.error(response)
-                logging.error(body)
-                await ctx.reply("Internal error")
-                return
-
-            json_response = await response.json()
-
-        if err := json_response.get("error") is not None:
-            await ctx.reply(f"Error from Pyromaniac backend: {err}")
-        else:
-            stdout: str = json_response.get("stdout")
-            stderr: str = json_response.get("stderr")
-            match (stdout, stderr):
-                case ("", ""):
-                    await ctx.reply("**Code executed succesfully, no output**")
-                case (_, ""):
-                    await ctx.reply(f"**Code Output**: \n ```\n{stdout}\n```")
-                case ("", _):
-                    await ctx.reply(f"**Code returned errors:**: \n ```\n{stderr}\n```")
-                case (_, _):
+                if response.content_type == "application/json":
+                    json_response = await response.json()
+                    await ctx.reply(json_response.get("error"))
+                else:
                     await ctx.reply(
-                        f"**Code returned both output and errors.**\n **Output:**\n{stdout}\n**Errors:**\n{stderr}"
+                        f"Internal error in pyromaniac:{await response.text()}"
                     )
-
-        assert self.bot.user
-        await ctx.message.remove_reaction("⏳", self.bot.user)
+            else:
+                json_response = await response.json()
+                stdout: str = json_response.get("stdout")
+                stderr: str = json_response.get("stderr")
+                match (stdout, stderr):
+                    case ("", ""):
+                        await ctx.reply("**Code executed succesfully, no output**")
+                    case (_, ""):
+                        await ctx.reply(f"**Code Output**: \n ```\n{stdout}\n```")
+                    case ("", _):
+                        await ctx.reply(
+                            f"**Code returned errors:**: \n ```\n{stderr}\n```"
+                        )
+                    case (_, _):
+                        await ctx.reply(
+                            f"**Code returned both output and errors.**\n **Output:**\n{stdout}\n**Errors:**\n{stderr}"
+                        )
 
 
 async def setup(bot: Bot):
