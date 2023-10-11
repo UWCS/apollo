39babb04dd438a7c28f8352aa8cd9661ca78eabf
Mole1424 <joel@joelcoulon.com>
2023-10-11 10:52:48 +0000

    simplified !say command

--- cogs/commands/say.py
+++ cogs/commands/say.py
@@ -1,5 +1,5 @@
 import discord
-from discord import AllowedMentions, app_commands
+from discord import AllowedMentions
 from discord.ext import commands
 from discord.ext.commands import Bot, Context, clean_content
 
@@ -14,15 +14,9 @@ class Say(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @app_commands.command(name="say", description=SHORT_HELP_TEXT)
-    async def say_slash(self, int: discord.Interaction, message: str):
-        await int.response.send_message(
-            message, allowed_mentions=AllowedMentions.none()
-        )
-
-    @commands.command(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
-    async def say(self, ctx: Context, *message: clean_content):
-        await ctx.send(" ".join([x.lstrip("@") for x in message]))
+    @commands.hybrid_command(help=LONG_HELP_TEXT, brief=SHORT_HELP_TEXT)
+    async def say(self, ctx: Context, *, message: clean_content):
+        await ctx.send(message)
 
 
 async def setup(bot: Bot):
