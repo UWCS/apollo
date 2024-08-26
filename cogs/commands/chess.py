d49d14fd68bf117c986a0dc9b00117f8bd6cc2cf
Ali Aljishi <ali.aljishi.2005@gmail.com>
2024-08-26 23:04:57 +0100

    select menu semi-functional for move selection, will try to switch to text input

--- cogs/commands/chess.py
+++ cogs/commands/chess.py
@@ -1,10 +1,9 @@
 from discord.ext import commands
 from discord.ext.commands import Bot, Context, clean_content
 
-from discord import File as discord_file
-from discord.ui import View as button_view
-from discord.ui import button as button, Button as Button
-from discord import Interaction
+from discord import File as discord_file, Interaction, SelectOption
+from discord.ui import View, Select, button, Button, select 
+from collections import defaultdict
 
 import chess.pgn
 import chess.svg
@@ -20,13 +19,69 @@ Make the bot repeat after you.
 
 SHORT_HELP_TEXT = """Make the bot repeat after you."""
 
+piece_mappings = {"B":"Bishop", "K": "King", "P": "Pawn", "N":"Knight", "R": "Rook", "Q":"Queen"}
 
-class ButtonInteractions(button_view): 
+
+
+# class RankSelection(Select):
+#     def __init__(self, legal_moves):
+#         moves = [SelectOption(label=piece_mappings[str(move)],value=move) for move in legal_moves]
+#         super().__init__(options=moves, placeholder="Select Piece", max_values=1)
+
+#     async def select_piece(self):
+
+
+class FileSelection(Select):
+    def __init__(self,legal_moves, piece):
+        
+        moves = [SelectOption(label=move,value=move) for move in legal_moves[piece]]
+        super().__init__(options=moves, placeholder="Select Piece", max_values=1)
+    
+
+
+
+class PieceSelection(Select):
+    def __init__(self,legal_moves):
+        self.legal_moves = legal_moves 
+        moves = [SelectOption(label=piece_mappings[move],value=move) for move in legal_moves.keys()]
+        super().__init__(options=moves, placeholder="Select Piece", max_values=1)
+
+    async def callback(self, interaction: Interaction):
+        self.placeholder = piece_mappings[self.values[0]]
+        await self.view.respond_piece(interaction, self.values[0], self.legal_moves)
+        await interaction.response.defer()
+        
+        
+        
+        
+
+
+
+
+class ButtonInteractions(View): 
     
     def __init__(self, board):
         super().__init__()
         self.board = board
         self.forward_stack = []
+
+    async def reload_board(self, interaction: Interaction):
+        svg_board = chess.svg.board(self.board)
+        bytesImage = BytesIO(svg2png(bytestring=svg_board))
+
+        exporter = chess.pgn.StringExporter(headers=True, variations=True, comments=True)
+        pgn = chess.pgn.Game.from_board(self.board).accept(exporter)
+
+        img = discord_file(bytesImage, filename="board.png", description=pgn)
+        await self.message.edit(view=self)
+        await interaction.response.edit_message(attachments=[img])
+
+    async def respond_piece(self, interaction: Interaction, select_items: Select, legal_moves):
+        self.children[2].disabled = True
+        file_selection = FileSelection(legal_moves, select_items)
+        self.add_item(file_selection)
+        await interaction.message.edit(view=self)
+
     @button(label="<-")
     async def left(self, interaction: Interaction, button : Button):
         if not self.board.move_stack:
@@ -41,14 +96,10 @@ class ButtonInteractions(button_view):
         self.forward_stack.append(self.board.peek())
         self.board.pop()
         self.children[1].disabled = False
-        svg_board = chess.svg.board(self.board)
-        bytesImage = BytesIO(svg2png(bytestring=svg_board))
-        exporter = chess.pgn.StringExporter(headers=True, variations=True, comments=True)
-        pgn = chess.pgn.Game.from_board(self.board).accept(exporter)
-        img = discord_file(bytesImage, filename="board.png", description=pgn)
-        await self.message.edit(view=self)
-        await interaction.response.edit_message(attachments=[img])
+        self.reload_board(interaction)
+    
 
+        
 
     @button(label="->")
     async def right(self, interaction: Interaction, button : Button):
@@ -64,13 +115,10 @@ class ButtonInteractions(button_view):
 
         self.board.push(self.forward_stack.pop())
         self.children[0].disabled = False
-        svg_board = chess.svg.board(self.board)
-        bytesImage = BytesIO(svg2png(bytestring=svg_board))
-        exporter = chess.pgn.StringExporter(headers=True, variations=True, comments=True)
-        pgn = chess.pgn.Game.from_board(self.board).accept(exporter)
-        img = discord_file(bytesImage, filename="board.png", description=pgn)
-        await self.message.edit(view=self)
-        await interaction.response.edit_message(attachments=[img])
+        
+        self.reload_board(interaction)
+
+
 
 
 class Chess(commands.Cog):
@@ -95,8 +143,14 @@ class Chess(commands.Cog):
         
         for move in game.mainline_moves():
             board.push(move)
-        
+        # [str(move)[0:2], str(move)[2:]]
+        legal_moves = defaultdict(list)
+        for move in board.legal_moves:
+            square = chess.parse_square(str(move)[:2])
+            piece = board.piece_at(square=square)
+            legal_moves[str(piece)].append(str(move))
         view = ButtonInteractions(board)
+        view.add_item(PieceSelection(legal_moves))
         svg_board = chess.svg.board(board)
         bytesImage = BytesIO(svg2png(bytestring=svg_board))
         img = discord_file(bytesImage, filename="board.png", description=pgn)
