fd37a1209cee06ec2b7f73754f447e0e359a3a12
Ali Aljishi <ali.aljishi.2005@gmail.com>
2024-08-28 17:37:43 +0100

    cleaning up, refactoring, and stylistic improvements

--- cogs/commands/chess.py
+++ cogs/commands/chess.py
@@ -20,16 +20,11 @@ Make the bot repeat after you.
 
 SHORT_HELP_TEXT = """Make the bot repeat after you."""
 
-piece_mappings = {"B":"Bishop", "K": "King", "P": "Pawn", "N":"Knight", "R": "Rook", "Q":"Queen"}
+piece_mappings = {"B":"Bishop", "K": "King", "P": "Pawn", "N":"Knight", "R": "Rook", "Q":"Queen",
+                    "b":"Bishop", "k": "King", "p": "Pawn", "n":"Knight", "r": "Rook", "q":"Queen"
+                }
 
 
-
-# class RankSelection(Select):
-#     def __init__(self, legal_moves):
-#         moves = [SelectOption(label=piece_mappings[str(move)],value=move) for move in legal_moves]
-#         super().__init__(options=moves, placeholder="Select Piece", max_values=1)
-
-#     async def select_piece(self):
 
 
 class FileSelection(Select):
@@ -39,32 +34,36 @@ class FileSelection(Select):
         
         self.page = -1
         
-        self.moves = [SelectOption(label=move,value=move) for move in legal_moves[piece]]
-
         
-        super().__init__(options=self.switch_page(), placeholder="Select a Square")
+        super().__init__(options=self.switch_page(legal_moves, piece), placeholder="Select a Square")
     
-    def switch_page(self):
+    def switch_page(self, legal_moves, piece):
+        self.generate_legal_moves_by_piece(legal_moves, piece)
         self.page = (self.page + 1) % ceil(len(self.moves)/self.items_per_page)    
         start = self.page * self.items_per_page
         end = start + self.items_per_page if len(self.moves) > start + self.items_per_page else len(self.moves)
-
         return self.moves[start:end]
-    
-    # async def callback(self, interaction: Interaction):
 
+    def generate_legal_moves_by_piece(self, legal_moves, piece):
+        self.moves = [SelectOption(label=move,value=move) for move in legal_moves[piece]] 
+        return self.moves
+    
+    async def callback(self, interaction: Interaction):
+        await self.view.play_move(self.values[0], interaction)
 
 
 
 class PieceSelection(Select):
     def __init__(self, legal_moves):
-        self.legal_moves = legal_moves
-        moves = [SelectOption(label=piece_mappings[move],value=move) for move in legal_moves.keys()]
-        super().__init__(options=moves, max_values=1)
+        super().__init__(options=self.generate_select_pieces(legal_moves), max_values=1, placeholder="Select Piece")
+    
+    def generate_select_pieces(self, legal_moves):
+        return [SelectOption(label=piece_mappings[move],value=move) for move in legal_moves.keys()]
+
 
     async def callback(self, interaction: Interaction):
         self.placeholder = piece_mappings[self.values[0]]
-        await self.view.respond_piece(interaction, self.values[0], self.legal_moves)
+        await self.view.select_piece_respond(interaction, self.values[0])
         await interaction.response.defer()
         
         
@@ -80,72 +79,105 @@ class MoveSelectionNext(Button):
         await interaction.response.edit_message(view=self.view)
 
 
-# class MoveSelectionBack(Button):        
-#     def __init__(self, view : ButtonInteractions):
-#         super().__init__(label="<")
-#         self.view = view
-
-#     async def callback(self, interaction: Interaction):
-#         self.view.page -= 1
-#         await interaction.response.edit_message(view=self.view)
-
-
-
-
-
 class ButtonInteractions(View): 
     
-    def __init__(self, board, legal_moves):
+    def __init__(self, board):
         super().__init__()
-        self.legal_moves = legal_moves
+        
         self.board = board
+        self.legal_moves = self.calculate_legal_moves()
+
+        self.prev_move_btn = self.children[0]
+        self.next_move_btn = self.children[1]
+        self.select_piece_menu = PieceSelection(self.legal_moves)
+        self.select_move_menu = None
+        self.switch_page_btn = None
+
+        self.add_item(self.select_piece_menu)
+
+        
         self.forward_stack = []
 
+    def calculate_legal_moves(self):
+        
+        legal_moves = defaultdict(list)
+        for move in self.board.legal_moves:
+            square = chess.parse_square(str(move)[:2])
+            piece = self.board.piece_at(square=square)
+            legal_moves[str(piece)].append(str(self.board.san(move)))
+        return legal_moves
+
     async def reload_board(self, interaction: Interaction):
+
         svg_board = chess.svg.board(self.board)
         bytesImage = BytesIO(svg2png(bytestring=svg_board))
 
         exporter = chess.pgn.StringExporter(headers=True, variations=True, comments=True)
         pgn = chess.pgn.Game.from_board(self.board).accept(exporter)
 
+
         img = discord_file(bytesImage, filename="board.png", description=pgn)
+        
+        self.legal_moves = self.calculate_legal_moves()
+        self.select_piece_menu.options = self.select_piece_menu.generate_select_pieces(self.legal_moves)
+        self.select_piece_menu.disabled = False
+        self.select_piece_menu.placeholder = "Select Piece"
+
+        if self.select_move_menu:
+            self.select_move_menu.page = -1
+            self.select_move_menu.disabled = True
+        if self.switch_page_btn: 
+            self.switch_page_btn.disabled = True
+        
+
         await self.message.edit(view=self)
         await interaction.response.edit_message(attachments=[img])
 
+
+    async def play_move(self, move, interaction : Interaction):
+        self.board.push(self.board.parse_san(move))
+        await self.reload_board(interaction)
+
     async def refresh_select_menu(self, piece):
+        self.select_move_menu.options = self.select_move_menu.switch_page(self.legal_moves, piece)
+
+    async def select_piece_respond(self, interaction: Interaction, piece: Select):
         
-
-
+        self.select_piece_menu.disabled = True
         
+        if not self.select_move_menu:
+            self.select_move_menu = FileSelection(self.legal_moves, piece)
+            self.add_item(self.select_move_menu)
+        else:
+            await self.refresh_select_menu(piece)
+        
+        self.select_move_menu.disabled = False
 
-        self.children[3].options = self.children[3].switch_page()
-
-    async def respond_piece(self, interaction: Interaction, piece: Select, legal_moves):
-        self.children[2].disabled = True
-        file_selection = FileSelection(self.legal_moves, piece)
-        self.add_item(file_selection)
-        if len(legal_moves[piece]) > 25:
-            self.add_item(MoveSelectionNext(piece))
+        if len(self.legal_moves[piece]) > 25:
+            if not self.switch_page_btn:
+                self.switch_page_btn = MoveSelectionNext(piece)
+                self.add_item(self.switch_page_btn)
+            self.switch_page_btn.disabled = False
+        else:
+            self.switch_page_btn = None
         await interaction.message.edit(view=self)
 
-    # async def update(self):
-    #     self.children
 
     @button(label="<-")
     async def left(self, interaction: Interaction, button : Button):
         if not self.board.move_stack:
             button.disabled = True
             if self.forward_stack:
-                self.children[1].disabled = False
+                self.next_move_btn.disabled = False
             else:
-                self.children[1].disabled = True
+                self.next_move_btn.disabled = True
             await self.message.edit(view=self)
             await interaction.response.edit_message()
             return
         self.forward_stack.append(self.board.peek())
         self.board.pop()
-        self.children[1].disabled = False
-        self.reload_board(interaction)
+        self.next_move_btn.disabled = False
+        await self.reload_board(interaction)
     
 
         
@@ -153,19 +185,19 @@ class ButtonInteractions(View):
     @button(label="->")
     async def right(self, interaction: Interaction, button : Button):
         if not self.forward_stack:
-            self.children[1].disabled = True;
+            button.disabled = True;
             if self.board.move_stack:
-                self.children[0].disabled = False
+                self.prev_move_btn.disabled = False
             else:
-                self.children[0].disabled = True
+                self.prev_move_btn.disabled = True
             await self.message.edit(view=self)
             await interaction.response.edit_message()
             return
 
         self.board.push(self.forward_stack.pop())
-        self.children[0].disabled = False
+        self.prev_move_btn.disabled = False
         
-        self.reload_board(interaction)
+        await self.reload_board(interaction)
 
 
 
@@ -192,15 +224,10 @@ class Chess(commands.Cog):
         
         for move in game.mainline_moves():
             board.push(move)
-        # [str(move)[0:2], str(move)[2:]]
-        legal_moves = defaultdict(list)
-        for move in board.legal_moves:
-            square = chess.parse_square(str(move)[:2])
-            piece = board.piece_at(square=square)
-            legal_moves[str(piece)].append(str(board.san(move)))
 
-        view = ButtonInteractions(board, legal_moves)
-        view.add_item(PieceSelection(legal_moves))
+        
+
+        view = ButtonInteractions(board)
         svg_board = chess.svg.board(board)
         bytesImage = BytesIO(svg2png(bytestring=svg_board))
         img = discord_file(bytesImage, filename="board.png", description=pgn)
