689f9461e95b5ff9cc97eea41946a99113c18ce3
Ali Aljishi <ali.aljishi.2005@gmail.com>
2024-11-07 00:53:34 +0000

    life's ruff

--- cogs/commands/onmessage.py
+++ cogs/commands/onmessage.py
@@ -5,6 +5,7 @@ from discord.abc import GuildChannel
 from discord.ext import commands
 from discord.ext.commands import Bot, Cog
 
+
 class OnMessage(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
