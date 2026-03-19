2bbd4290a09d4c924370e9fd8f037aef607a6a44
lunazeta <qwhaty@hotmail.com>
2026-03-19 14:45:30 +0000

    Made recommended changes based on #296

--- cogs/bot_moderation.py
+++ cogs/bot_moderation.py
@@ -4,8 +4,11 @@ import discord
 from discord import Color, Embed
 from discord.ext.commands import Bot, Cog
 
+from pytz import timezone
+
 from config import CONFIG
 
+LONDON = timezone("Europe/London")
 
 class BotModeration(Cog):
     def __init__(self, bot: Bot):
@@ -13,9 +16,9 @@ class BotModeration(Cog):
         
     @Cog.listener()
     async def on_message(self, message: discord.Message):
-        joined_recently = message.author.joined_at > datetime.now() - timedelta(days=7)
-        contains_everyone = '@everyone' in message.content
-        is_giving_away = 'giving away' in message.content.lower()
+        joined_recently = message.author.joined_at > datetime.now(LONDON) - timedelta(days=7)
+        contains_everyone = "@everyone" in message.content
+        is_giving_away = "giving away" in message.content.lower()
 
         if not joined_recently or not (contains_everyone or is_giving_away):
             return
@@ -23,15 +26,15 @@ class BotModeration(Cog):
         channel = self.bot.get_channel(CONFIG.UWCS_BOT_LOG_CHANNEL_ID)
 
         embed_colour = Color.from_rgb(61, 83, 255)
-        embed_title = f'@{message.author.global_name}, ID: {message.author.id}'
-        embed_description = f'User suspected to be a bot, joined_recently: {joined_recently}, contains_everyone: {contains_everyone}, is_giving_away: {is_giving_away}'
+        embed_title = f"@{message.author.name}, ID: {message.author.id}"
+        embed_description = f"User suspected to be a bot, joined_recently: {joined_recently}, contains_everyone: {contains_everyone}, is_giving_away: {is_giving_away}"
         embed = Embed(
-            title=embed_title, color=embed_colour, embed_description=embed_description
+            title=embed_title, color=embed_colour, description=embed_description
         )
         
         await message.delete()
-        await channel.send(f'{' '.join([f'<@&{exec_role}>' for exec_role in CONFIG.UWCS_EXEC_ROLE_IDS])}', embed=embed)
+        await channel.send(" ".join([f"<@&{exec_role}>" for exec_role in CONFIG.UWCS_EXEC_ROLE_IDS]), embed=embed)
         await message.author.timeout(timedelta(days=1))
 
 async def setup(bot: Bot):
-    await bot.add_cog(Database(bot))
+    await bot.add_cog(BotModeration(bot))
