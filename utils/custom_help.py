d550afed07f0eb533f5d8a6d87249b4f0d40977e
raven0034 <monkeyboy2805@gmail.com>
2026-04-24 17:05:37 +0100

    Tidy up and doc

--- utils/custom_help.py
+++ utils/custom_help.py
@@ -27,17 +27,25 @@ class SimplePrettyHelp(commands.HelpCommand):
                 }
             )
 
-        await self.get_destination().send(
-            embeds=[discord.Embed.from_dict(
-                {
-                    "color": self.color,
-                    "fields": cog_fields[i:i+25],
-                    "footer": {
-                        "text": "For more information on a command : !help [command]"
-                    },
-                }
-            ) for i in range(0, len(cog_fields), 25)]
-        )
+        ## embeds can only have 25 fields
+        ## per https://docs.discord.com/developers/resources/message#embed-object
+        embeds = []
+        for i in range(0, len(cog_fields), 25):
+            ## create embeds for each slice of 25
+            embeds.append(
+                discord.Embed.from_dict(
+                    {
+                        "color": self.color,
+                        "fields": cog_fields[i:i+25],
+                        "footer": {
+                            "text": "For more information on a command : !help [command]"
+                        },
+                    }
+                )
+            )
+
+        ## send all embeds
+        await self.get_destination().send(embeds=embeds)
 
     async def send_command_help(self, command):
         """Command help menu"""
